4f7bb8b99f3275373e4befd1e8ce276f79280bd5
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-23 18:22:05 +0300

    Update Car.java

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,44 @@
 package core.basesyntax;
 
-public class Car {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class Car implements Cloneable, Serializable {
+    private Engine engine;
+    private String number;
+    private ArrayList<String> documentation;
+    private HashMap<String, Integer> map;
+    private final int MAX_SPEED;
+
+    private int getSpeed() {
+        return this.MAX_SPEED;
+    }
+
+    public Car(int maxSpeed, ArrayList<String> documentation,
+               String number, Engine engine, HashMap<String, Integer> map) {
+        this.MAX_SPEED = maxSpeed;
+        this.documentation = documentation;
+        this.number = number;
+        this.engine = engine;
+        this.map = map;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        Car clone = null;
+        try {
+            clone = (Car) super.clone();
+            clone.engine = new Engine(this.engine);
+            clone.documentation = new ArrayList<>(this.documentation);
+            clone.map = new HashMap<>(this.map);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
