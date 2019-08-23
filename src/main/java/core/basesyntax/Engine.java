6160281b83837be6179271d7d40a88d6c1227ad8
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-23 18:31:23 +0300

    Update Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public final class Engine {
+    private final String cylinderDiameter;
+    private final String pistonStroke;
+    private final String crankRadius;
+    private final String capacity;
+    private final String compressionRatio;
 
+    public Engine(String cylinderDiameter, String pistonStroke,
+                  String crankRadius, String capacity, String compressionRatio) {
+        this.cylinderDiameter = cylinderDiameter;
+        this.pistonStroke = pistonStroke;
+        this.crankRadius = crankRadius;
+        this.capacity = capacity;
+        this.compressionRatio = compressionRatio;
+    }
+
+    public Engine(Engine engine) {
+        this.cylinderDiameter = engine.cylinderDiameter;
+        this.pistonStroke = engine.pistonStroke;
+        this.crankRadius = engine.crankRadius;
+        this.capacity = engine.capacity;
+        this.compressionRatio = engine.compressionRatio;
+    }
 }
