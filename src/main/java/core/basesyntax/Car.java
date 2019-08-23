f3b35deb45c244191893425e23f9d4d3fd8bb2ed
roman_minevich <283389dv3qsh>
2019-08-23 19:58:48 +0300

    Added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String type;
+    private String manufacturer;
+    private int productionYear;
+    private int speed;
+    private Engine engine;
 
+    @Override
+    public Car clone() {
+        Car car = new Car();
+        car.type = type;
+        car.manufacturer = manufacturer;
+        car.productionYear = productionYear;
+        car.speed = speed;
+        car.engine = engine;
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String manufacturer;
+    private String supply;
+    private int power;
+    private char type;
+    private boolean condition;
 
+    @Override
+    public Engine clone() {
+         Engine engine = new Engine();
+         engine.manufacturer = manufacturer;
+         engine.supply = supply;
+         engine.power = power;
+         engine.type = type;
+         engine.condition = condition;
+         return engine;
+    }
 }
