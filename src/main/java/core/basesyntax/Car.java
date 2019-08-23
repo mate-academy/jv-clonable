95a228daaea3edd5e8260936cd3262b2dcd0e28f
AndreyDMA <andreydmitriev@ukr.net>
2019-08-23 14:17:33 +0300

    first try solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable{
+    private String name;
+    private String color;
+    private String type;
+    private int doorsQnty;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String name, String color, String type, int doorsQnty, int maxSpeed) {
+        this.name = name;
+        this.color = color;
+        this.type = type;
+        this.doorsQnty = doorsQnty;
+        this.maxSpeed = maxSpeed;
+
+        Engine engine = new Engine();
+        engine.installEngine();
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(this.name, this.color, this.type, this.doorsQnty, this.maxSpeed);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
 public class Engine {
+    private String fuelType;
+    private double volume;
+    private double fuelCons;
+    private int numberOfCyl;
+    private int maxTorque;
 
+    public Engine () {
+
+    }
+
+    public Engine(String fuelType, double volume, double fuelCons, int numberOfCyl, int maxTorque) {
+        this.fuelType = fuelType;
+        this.volume = volume;
+        this.fuelCons = fuelCons;
+        this.numberOfCyl = numberOfCyl;
+        this.maxTorque = maxTorque;
+
+    }
+
+    protected void installEngine() {
+        System.out.println("Engine is installed");
+    }
 }
