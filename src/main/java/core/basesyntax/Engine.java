1978884090c1ff0315b3ee42e03999d7f31d8c3a
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-23 13:22:09 +0300

    Update Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,64 @@
 package core.basesyntax;
 
-public class Engine {
+import java.util.Date;
 
+public class Engine implements Cloneable {
+
+    private EngineType engineType;
+    private int maxRpM;
+    private String brand;
+    private String model;
+    private final Date issue;
+
+    public Engine(final EngineType engineType, final int maxRpM,
+                  final String brand, final String model, final Date issue) {
+        this.engineType = engineType;
+        this.maxRpM = maxRpM;
+        this.brand = brand;
+        this.model = model;
+        this.issue = issue;
+    }
+
+    public EngineType getEngineType() {
+        return this.engineType;
+    }
+
+    public void setEngineType(final EngineType engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getMaxRpM() {
+        return this.maxRpM;
+    }
+
+    public void setMaxRpM(final int maxRpM) {
+        this.maxRpM = maxRpM;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(final String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(final String model) {
+        this.model = model;
+    }
+
+    public Date getIssue() {
+        return this.issue;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    static enum EngineType { HeatEngine, ElectricEngine, AirEngine }
 }
