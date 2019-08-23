1ff8d91e5b7a7bda9881748b8df47ba9d2c5e468
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-23 19:05:34 +0300

    Update Engine.java

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
-public final class Engine {
+import java.util.HashMap;
+
+public final class Engine implements Cloneable {
     private final String cylinderDiameter;
     private final String pistonStroke;
     private final String crankRadius;
     private final String capacity;
-    private final String compressionRatio;
+    private final HashMap<String, Integer> compressionRatio;
 
     public Engine(String cylinderDiameter, String pistonStroke,
-                  String crankRadius, String capacity, String compressionRatio) {
+                  String crankRadius, String capacity, HashMap<String, Integer> compressionRatio) {
         this.cylinderDiameter = cylinderDiameter;
         this.pistonStroke = pistonStroke;
         this.crankRadius = crankRadius;
@@ -22,5 +24,10 @@ public final class Engine {
         this.crankRadius = engine.crankRadius;
         this.capacity = engine.capacity;
         this.compressionRatio = engine.compressionRatio;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(this);
     }
 }
