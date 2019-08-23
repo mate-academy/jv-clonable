6c08733549e625001b3ef1a1d3f131ff8b9ba212
AndreyDMA <andreydmitriev@ukr.net>
2019-08-23 19:28:49 +0300

    second try solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class Car implements Cloneable{
+public class Car implements Cloneable {
     private String name;
     private String color;
     private String type;
@@ -8,20 +8,19 @@ public class Car implements Cloneable{
     private int maxSpeed;
     private Engine engine;
 
-    public Car(String name, String color, String type, int doorsQnty, int maxSpeed) {
+    public Car(String name, String color, String type, int doorsQnty, int maxSpeed, Engine engine) {
         this.name = name;
         this.color = color;
         this.type = type;
         this.doorsQnty = doorsQnty;
         this.maxSpeed = maxSpeed;
-
-        Engine engine = new Engine();
-        engine.installEngine();
+        this.engine = engine;
     }
 
     @Override
     protected Car clone() throws CloneNotSupportedException {
-        return new Car(this.name, this.color, this.type, this.doorsQnty, this.maxSpeed);
+        return new Car(this.name, this.color, this.type, this.doorsQnty, this.maxSpeed,
+                engine.clone());
     }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,15 +1,11 @@
 package core.basesyntax;
 
-public class Engine implements Cloneable{
+public class Engine implements Cloneable {
     private String fuelType;
     private double volume;
     private double fuelCons;
     private int numberOfCyl;
     private int maxTorque;
-
-    public Engine () {
-
-    }
 
     public Engine(String fuelType, double volume, double fuelCons, int numberOfCyl, int maxTorque) {
         this.fuelType = fuelType;
@@ -17,16 +13,12 @@ public class Engine implements Cloneable{
         this.fuelCons = fuelCons;
         this.numberOfCyl = numberOfCyl;
         this.maxTorque = maxTorque;
-
-    }
-
-    protected void installEngine() {
-        System.out.println("Engine is installed");
     }
 
     @Override
     protected Engine clone() throws CloneNotSupportedException {
-        return new Engine(this.fuelType, this.volume, this.fuelCons, this.numberOfCyl, this.maxTorque);
+        return new Engine(this.fuelType, this.volume,
+                this.fuelCons, this.numberOfCyl, this.maxTorque);
     }
 
 }
