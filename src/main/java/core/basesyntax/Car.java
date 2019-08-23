8825380ff52b607e55c445848b55da61ff72de56
Kokhanevich <nike.k1995@gmail.com>
2019-08-23 15:06:08 +0300

    added solution for HW5-clonable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,73 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String colour;
+    private int maxSpeed;
+    private int  peopleCapacity;
+    private int yearBuild;
+    private Engine engine;
 
+    public Car(String model, Engine engine, String colour,
+               int maxSpeed, int peopleCapacity, int yearBuild) {
+        this.model = model;
+        this.colour = colour;
+        this.maxSpeed = maxSpeed;
+        this.peopleCapacity = peopleCapacity;
+        this.yearBuild = yearBuild;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getPeopleCapacity() {
+        return peopleCapacity;
+    }
+
+    public void setPeopleCapacity(int peopleCapacity) {
+        this.peopleCapacity = peopleCapacity;
+    }
+
+    public int getYearBuild() {
+        return yearBuild;
+    }
+
+    public void setYearBuild(int yearBuild) {
+        this.yearBuild = yearBuild;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(model,engine,colour,maxSpeed,peopleCapacity,yearBuild);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String modelEngine;
+    private String type;
+    private int enginePower;
+    private int costEngine;
+    private double engineCapacity;
 
+    public Engine(String modelEngine, String type,
+                  int enginePower, int costEngine, double engineCapacity) {
+        this.modelEngine = modelEngine;
+        this.type = type;
+        this.enginePower = enginePower;
+        this.costEngine = costEngine;
+        this.engineCapacity = engineCapacity;
+    }
+
+    public String getModelEngine() {
+        return modelEngine;
+    }
+
+    public void setModelEngine(String modelEngine) {
+        this.modelEngine = modelEngine;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getCostEngine() {
+        return costEngine;
+    }
+
+    public void setCostEngine(int costEngine) {
+        this.costEngine = costEngine;
+    }
+
+    public double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(modelEngine, type,enginePower,costEngine,engineCapacity);
+    }
 }
