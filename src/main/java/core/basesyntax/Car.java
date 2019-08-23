a8b2c35758803fa567243daf6bf6acbba04c0406
kmety <kmety9876@gmail.com>
2019-08-23 20:29:30 +0300

    solution added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,56 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private double price;
+    private double weight;
+    private Engine engine;
 
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car) super.clone();
+        clonedCar.setEngine(this.engine.clone());
+        return clonedCar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double power;
+    private double capacity;
+    private double fuelConsumption;
+    private double oilConsumption;
+    private int numberOfValves;
 
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getOilConsumption() {
+        return oilConsumption;
+    }
+
+    public void setOilConsumption(double oilConsumption) {
+        this.oilConsumption = oilConsumption;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public void setNumberOfValves(int numberOfValves) {
+        this.numberOfValves = numberOfValves;
+    }
 }
