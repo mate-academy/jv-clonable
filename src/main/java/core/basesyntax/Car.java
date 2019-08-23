210e669e478da6b50e89aa397c16b400524756ad
Korfat <38156885+Korfat@users.noreply.github.com>
2019-08-23 18:47:11 +0300

    Added solution for clonable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,84 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String type;
+    private String color;
+    private int maxSpeed;
+    private int numOfSeats;
+    private Engine engine;
 
+    public Car() {
+
+    }
+
+    public Car(String name, String type, String color,
+               int maxSpeed, int numOfSeats, Engine engine) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.numOfSeats = numOfSeats;
+        this.engine = engine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(int numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car clone = new Car();
+        clone.setName(this.name);
+        clone.setType(this.type);
+        clone.setColor(this.color);
+        clone.setMaxSpeed(this.maxSpeed);
+        clone.setNumOfSeats(this.numOfSeats);
+        clone.setEngine(this.engine.clone());
+        return clone;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,72 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private String fuel;
+    private double capacity;
+    private double weight;
+    private int power;
 
+    public Engine() {
+
+    }
+
+    public Engine(String name, String fuel, double capacity, double weight, int power) {
+        this.name = name;
+        this.fuel = fuel;
+        this.capacity = capacity;
+        this.weight = weight;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        Engine engine = new Engine();
+        engine.setName(this.name);
+        engine.setFuel(this.fuel);
+        engine.setCapacity(this.capacity);
+        engine.setWeight(this.weight);
+        engine.setPower(this.power);
+        return engine;
+    }
 }
