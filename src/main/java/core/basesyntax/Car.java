98797332e9e0b0aa37da28190c079a0f44c1c850
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-23 15:59:34 +0300

    Car and Engine classes finished, method clone added to each

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String registrationNumber;
+    private String color;
+    private Engine engine;
+    private int age;
 
+    public Car(String name, String registrationNumber, String color, Engine engine, int age) {
+        this.name = name;
+        this.registrationNumber = registrationNumber;
+        this.color = color;
+        this.engine = engine;
+        this.age = age;
+    }
+
+    public Car clone() {
+        return new Car(new String(name), new String(registrationNumber),
+                new String(color), engine.clone(), age);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int horsePower;
+    private int mileage;
+    private int weight;
+    private int age;
 
+    Engine(String type, int horsePower, int mileage, int weight, int age) {
+        this.type = type;
+        this.horsePower = horsePower;
+        this.mileage = mileage;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public Engine clone() {
+        return new Engine(type, horsePower, mileage, weight, age);
+    }
 }
