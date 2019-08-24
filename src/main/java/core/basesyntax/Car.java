6bb33dcec4825d7af631e59b6888458632523114
lenovo <suleimanovayev@gmail.com>
2019-08-24 19:32:08 +0300

    solution clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,30 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable{
+    private String model;
+    private String color;
+    private int dateOfIssue;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String model, String color, int date, int maxSpeed, Engine engine1) {
+        this.model = model;
+        this.color = color;
+        this.dateOfIssue = date;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine1;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setModel(int dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+    public Car clone() throws CloneNotSupportedException {
+        return (Car) super.clone();
+    }
 
 }
