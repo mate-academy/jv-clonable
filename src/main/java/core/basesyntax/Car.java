5871475a4076203abbb4c83a3d39728badb862b9
TatianaPirch <pirch.tatiana@gmail.com>
2019-08-24 22:22:53 +0300

    car cloning solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,66 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private double mileage;
+    private int speed;
+    private String color;
+    private Engine engine;
+
+    public Car(String model, double mileage, int speed, String color, Engine engine) {
+        this.model = model;
+        this.mileage = mileage;
+        this.speed = speed;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car)super.clone();
+        Engine engine = this.getEngine().clone();
+        newCar.setEngine(engine);
+        return newCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int maxRpm;
+    private int horsePower;
+    private double weight;
+    private String model;
+    private int manufactureYear;
 
+    public Engine(int maxRpm, int horsePower, double weight, String model, int manufactureYear) {
+        this.maxRpm = maxRpm;
+        this.horsePower = horsePower;
+        this.weight = weight;
+        this.model = model;
+        this.manufactureYear = manufactureYear;
+    }
+
+    public int getMaxRpm() {
+        return maxRpm;
+    }
+
+    public void setMaxRpm(int maxRpm) {
+        this.maxRpm = maxRpm;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
