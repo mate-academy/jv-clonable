2a36c7dcb540508ec237c1e01ce77e1d48e9166e
ghkhrshch <george.khruschov@gmail.com>
2019-08-24 22:24:21 +0300

    Solution added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,77 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private int numberOfSeats;
+    private int weightCapacity;
+    private boolean spareTire;
+    private Engine engine;
 
+    public Car(String model, String color, int numberOfSeats, int weightCapacity,
+               boolean spareTire, Engine engine) {
+        this.model = model;
+        this.color = color;
+        this.numberOfSeats = numberOfSeats;
+        this.weightCapacity = weightCapacity;
+        this.spareTire = spareTire;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getWeightCapacity() {
+        return weightCapacity;
+    }
+
+    public void setWeightCapacity(int weightCapacity) {
+        this.weightCapacity = weightCapacity;
+    }
+
+    public boolean isSpareTire() {
+        return spareTire;
+    }
+
+    public void setSpareTire(boolean spareTire) {
+        this.spareTire = spareTire;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        Car clonedCar =  new Car(this.getModel(), this.getColor(),
+                this.getNumberOfSeats(), this.getWeightCapacity(),
+                this.isSpareTire(), this.getEngine());
+        clonedCar.engine = (Engine) this.engine.clone();
+        return clonedCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String serialnumber;
+    private double weight;
+    private double power;
+    private double maxRotationFreq;
 
+    public Engine(String model, String serialnumber, double weight,
+                  double power, double maxRotationFreq) {
+        this.model = model;
+        this.serialnumber = serialnumber;
+        this.weight = weight;
+        this.power = power;
+        this.maxRotationFreq = maxRotationFreq;
+    }
+
+    public Engine() {
+        System.out.println("Engine installed");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialnumber() {
+        return serialnumber;
+    }
+
+    public void setSerialnumber(String serialnumber) {
+        this.serialnumber = serialnumber;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getMaxRotationFreq() {
+        return maxRotationFreq;
+    }
+
+    public void setMaxRotationFreq(double maxRotationFreq) {
+        this.maxRotationFreq = maxRotationFreq;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(this.getModel(), this.getSerialnumber(),
+                this.getWeight(), this.getPower(), this.getMaxRotationFreq());
+    }
 }
