46b752642a6b1b974012b25d435b404f06462369
Valdes2 <52889938+Valdes2@users.noreply.github.com>
2019-08-24 10:49:15 +0300

    added solution for homework Clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,45 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private final String name;
+    private final String model;
+    private final int weight;
+    private final int maxSpeed;
+    private final int price;
+    private Engine engine;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Car(String name, String model, int weight, int maxSpeed, int price, Engine engine) {
+        this.name = name;
+        this.model = model;
+        this.weight = weight;
+        this.maxSpeed = maxSpeed;
+        this.price = price;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private final int capacity;
+    private final int power;
+    private final int torque;
+    private final int fuelConsumption;
+    private final String fuelType;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public Engine(int capacity, int power, int torque, int fuelConsumption, String fuelType) {
+        this.capacity = capacity;
+        this.power = power;
+        this.torque = torque;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
