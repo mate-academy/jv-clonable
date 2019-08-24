2fb7b49ae904a8543a80b265da09060d2dd20ae2
lenovo <suleimanovayev@gmail.com>
2019-08-24 23:44:25 +0300

    solution clo

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,37 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private String fuelUsed;
+    private String type;
+    private int capacity;
+    private int power;
 
+    public Engine(String name, String fuel, String type, int capacity, int power) {
+        this.name = name;
+        this.fuelUsed = fuel;
+        this.type = type;
+        this.capacity = capacity;
+        this.power = power;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFuelUsed(String fuelUsed) {
+        this.fuelUsed = fuelUsed;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
