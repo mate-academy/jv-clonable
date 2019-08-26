f7592a4126cd751327187996fe456802da9bde9a
Sviatoslav <s.granovskiy@gmail.com>
2019-08-26 20:13:17 +0300

    hw-5.1-solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,113 @@
 package core.basesyntax;
 
-public class Car {
+import java.util.Objects;
 
+public class Car implements Cloneable {
+    private int age;
+    private int maxSpeed;
+    private String modelName;
+    private String color;
+    private String manufacturer;
+    private Engine engine;
+
+    public Car(int age, int maxSpeed, String modelName, String color,
+               String manufacturer, Engine engine) {
+        this.age = age;
+        this.maxSpeed = maxSpeed;
+        this.modelName = modelName;
+        this.color = color;
+        this.manufacturer = manufacturer;
+        this.engine = engine;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Car{"
+                + "age=" + age
+                + ", maxSpeed=" + maxSpeed
+                + ", modelName='" + modelName + '\''
+                + ", color='" + color + '\''
+                + ", manufacturer='" + manufacturer + '\''
+                + ", engine=" + engine
+                + '}';
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        if (!super.equals(object)) {
+            return false;
+        }
+        Car car = (Car) object;
+        return getAge() == car.getAge()
+                && getMaxSpeed() == car.getMaxSpeed()
+                && java.util.Objects.equals(getModelName(), car.getModelName())
+                && java.util.Objects.equals(getColor(), car.getColor())
+                && java.util.Objects.equals(getManufacturer(), car.getManufacturer())
+                && java.util.Objects.equals(getEngine(), car.getEngine());
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getAge(), getMaxSpeed(), getModelName(),
+                getColor(), getManufacturer(), getEngine());
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Engine engine = new Engine(1215, 300, 2, "Honda", "petrol");
+        Car car = new Car(3, 220, "Civic", "white", "Honda", engine);
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,75 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int number;
+    private int horsePower;
+    private int volume;
+    private String manufacturer;
+    private String type;
 
+    public Engine(int number, int horsePower, int volume, String manufacturer, String type) {
+        this.number = number;
+        this.horsePower = horsePower;
+        this.volume = volume;
+        this.manufacturer = manufacturer;
+        this.type = type;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "number=" + number
+                + ", horsePower=" + horsePower
+                + ", volume=" + volume
+                + ", manufacturer='" + manufacturer + '\''
+                + ", type='" + type + '\''
+                + '}';
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        super.clone();
+        Engine engine = new Engine(1215, 300, 2, "Honda", "petrol");
+        return engine;
+    }
 }
