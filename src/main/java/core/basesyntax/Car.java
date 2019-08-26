c352086132bcbdf06eaca0d5afcc54363cf01f42
Ruslan <vip.russle@gmail.com>
2019-08-26 16:17:47 +0300

    added clone for Car and Engine

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String carLabel;
+    private String carType;
+    private int height;
+    private int width;
+    private int length;
+    private Engine engine;
+
+    public Car(String carLabel, String carType, int height, int width, int length, Engine engine) {
+        this.carLabel = carLabel;
+        this.carType = carType;
+        this.height = height;
+        this.width = width;
+        this.length = length;
+        this.engine = engine;
+    }
+
+    public Car() {
+
+    }
+
+    public void setCarLabel(String carLabel) {
+        this.carLabel = carLabel;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Engine engine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        Car clone = new Car();
+        clone.length = this.length;
+        clone.width = this.width;
+        clone.height = this.height;
+        clone.setCarLabel(this.carLabel);
+        clone.setCarType(this.carType);
+        clone.setEngine(this.engine);
+        return clone;
+
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,52 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int amount;
+    private String type;
+    private int maxSpeed;
+    private int dispersal;
+    private boolean isThereColdSys;
 
+    public Engine() {
+
+    }
+
+    public Engine(int amount, String type, int maxSpeed, int dispersal, boolean isThereColdSys) {
+        this.amount = amount;
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+        this.dispersal = dispersal;
+        this.isThereColdSys = isThereColdSys;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setDispersal(int dispersal) {
+        this.dispersal = dispersal;
+    }
+
+    public void setThereColdSys(boolean thereColdSys) {
+        isThereColdSys = thereColdSys;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        Engine clone = new Engine();
+        clone.isThereColdSys = this.isThereColdSys;
+        clone.dispersal = this.dispersal;
+        clone.amount = this.amount;
+        clone.setType(this.type);
+        clone.maxSpeed = maxSpeed;
+        return clone;
+    }
 }
