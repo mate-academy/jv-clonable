18b5df7d16b1528af1698e35183b4d10da54e648
Rosinskyi <sergey22061993@gmail.com>
2019-08-26 13:23:58 +0300

    added solution for jv-cloneable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private String type;
+    private int topSpeed;
+    private int mileage;
+    private Engine engine;
 
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car car = new Car();
+        car.name = this.name;
+        car.color = this.color;
+        car.type = this.type;
+        car.topSpeed = this.topSpeed;
+        car.mileage = this.mileage;
+        return car;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private boolean startEngine(Engine engine) {
+        return true;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,60 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean status;
+    private double valume;
+    private String engineType;
+    private double temperature;
+    private int numberOfPiston;
 
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        Engine engine = new Engine();
+        engine.status = this.status;
+        engine.valume = this.valume;
+        engine.engineType = this.engineType;
+        engine.temperature = this.temperature;
+        engine.numberOfPiston = this.numberOfPiston;
+        return engine;
+    }
+
+    public double getValume() {
+        return valume;
+    }
+
+    public void setValume(double valume) {
+        this.valume = valume;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getNumberOfPiston() {
+        return numberOfPiston;
+    }
+
+    public void setNumberOfPiston(int numberOfPiston) {
+        this.numberOfPiston = numberOfPiston;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
