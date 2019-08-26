f558b111922f6bffa3cb1184bc331864e728a68c
Max <skaterweather41@gmail.com>
2019-08-26 21:55:03 +0200

    added solution to task 1 clonable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,64 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String transmission;
+    private String type;
+    private String wheels;
+    private int seats;
 
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car) super.clone();
+        clonedCar.setEngine(this.engine.clone());
+        return clonedCar;
+    }
+
+    public Car(Engine engine, String transmission, String type, String wheels, int seats) {
+        this.engine = engine;
+        this.transmission = transmission;
+        this.type = type;
+        this.wheels = wheels;
+        this.seats = seats;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWeels() {
+        return wheels;
+    }
+
+    public void setWeels(String weels) {
+        this.wheels = weels;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private int power;
+    private int compression;
+    private int volume;
+    private int quantityOfPistols;
 
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
+
+    public Engine(String model, int power, int compression, int volume, int quantityOfPistols) {
+        this.model = model;
+        this.power = power;
+        this.compression = compression;
+        this.volume = volume;
+        this.quantityOfPistols = quantityOfPistols;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getCompression() {
+        return compression;
+    }
+
+    public void setCompression(int compression) {
+        this.compression = compression;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getQuantatyOfPistols() {
+        return quantityOfPistols;
+    }
+
+    public void setQuantatyOfPistols(int quantityOfPistols) {
+        this.quantityOfPistols = quantityOfPistols;
+    }
 }
