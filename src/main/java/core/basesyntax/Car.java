c726f4c9ba8e375a61571f91da6ac706428374a3
Taras Khalak <tarasulo@gmail.com>
2019-08-26 20:32:42 +0300

    'Solution'

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,77 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private String color;
+    private int warranty;
+    private boolean insurance;
+    private Engine engine;
 
+    public Car(String brand, String model,
+               String color, int warranty, boolean insurance) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.warranty = warranty;
+        this.insurance = insurance;
+    }
+
+    public Car() {
+        System.out.println("Hi to the new car!");
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWarranty() {
+        return warranty;
+    }
+
+    public boolean isInsurance() {
+        return insurance;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWarranty(int warranty) {
+        this.warranty = warranty;
+    }
+
+    public void setInsurance(boolean insurance) {
+        this.insurance = insurance;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(this.getBrand(), this.getModel(),
+                this.getColor(), this.getWarranty(), this.isInsurance());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String serialNumber;
+    private String fuel;
+    private int maxSpeed;
+    private double capacity;
 
+    public Engine(String model, String serialNumber,
+                  String fuel, int maxSpeed, double capacity) {
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.fuel = fuel;
+        this.maxSpeed = maxSpeed;
+        this.capacity = capacity;
+    }
+
+    public Engine() {
+        System.out.println("The engine has been installed");
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return new Engine(this.getModel(), this.getSerialNumber(),
+                this.getFuel(), this.getMaxSpeed(), this.getCapacity());
+    }
 }
