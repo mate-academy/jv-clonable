1414247b3a7b335994c971c29b626b8095eade04
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-27 11:32:40 +0300

    added solution for task with clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String modelName;
+    private String type;
+    private Engine engine;
+    private int seats;
+    private int maxSpeed;
 
+    public Car(String modelName, String type, Engine engine, int seats, int maxSpeed) {
+        this.modelName = modelName;
+        this.type = type;
+        this.engine = engine;
+        this.seats = seats;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(modelName, type, engine.clone(), seats, maxSpeed);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String modelName;
+    private String type;
+    private int volume;
+    private int power;
+    private int weight;
 
+    public Engine(String modelName, String type, int volume, int power, int weight) {
+        this.modelName = modelName;
+        this.type = type;
+        this.volume = volume;
+        this.power = power;
+        this.weight = weight;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
