b442674f1929e3798170e7129d2a64e222671226
Andrew <tolstuy1996@gmail.com>
2019-08-27 09:05:34 +0300

    clone made

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,56 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private String kind;
+    private int price;
+    private Engine engine;
 
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car clone = (Car) super.clone();
+        clone.engine = this.engine.clone();
+        return clone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double power;
+    private double capacity;
+    private double fuel;
+    private double oil;
+    private int IdNumber ;
 
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setOil(double oil) {
+        this.oil = oil;
+    }
+
+    public void setIdNumber(int idNumber) {
+        IdNumber = idNumber;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public double getOil() {
+        return oil;
+    }
+
+    public int getIdNumber() {
+        return IdNumber;
+    }
 }
