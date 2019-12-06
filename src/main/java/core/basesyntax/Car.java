cc7ffaa3891e843a3a721d459b5f760677904e40
Serhii Yevtushok <ggazila@gmail.com>
2019-12-06 19:13:12 +0200

    solution to hw-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,70 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private int year;
+    private int maxSpeed;
+    private boolean isNew;
+    private String model;
 
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Car setEngine(Engine engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Car setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public Car setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+        return this;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public Car setNew(boolean isNew) {
+        this.isNew = isNew;
+        return this;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Car setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public Car(Engine engine, int year, int maxSpeed, boolean isNew, String model) {
+        this.engine = engine;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.isNew = isNew;
+        this.model = model;
+    }
+
+    @Override
+    protected Object clone()
+            throws CloneNotSupportedException {
+        Engine cloneEngine = engine.clone();
+        Car cloneCar = new Car(getEngine(), getYear(), getMaxSpeed(), isNew, getModel());
+        return cloneCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String type;
+    private int price;
+    private int year;
+    private boolean isNew;
 
+    public String getModel() {
+        return model;
+    }
+
+    public Engine setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Engine setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Engine setPrice(int price) {
+        this.price = price;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Engine setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public Engine setNew(boolean isNew) {
+        this.isNew = isNew;
+        return this;
+    }
+
+    public Engine(String model, String type, int price, int year, boolean isNew) {
+        this.model = model;
+        this.type = type;
+        this.price = price;
+        this.year = year;
+        this.isNew = isNew;
+    }
+
+    @Override
+    protected Engine clone()
+            throws CloneNotSupportedException {
+        return new Engine(model, type, price, year, isNew);
+    }
 }
