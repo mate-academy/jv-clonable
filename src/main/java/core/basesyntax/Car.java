94b2ec632501b990b191e961f546e1869c88d027
SergiyAgeev <sergiyageev@gmail.com>
2019-12-06 13:41:55 +0200

    hw 5 1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,75 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String markName;
+    private int maxSpeed;
+    private int age;
+    private String color;
+    private Engine engine;
 
+    public Car(String markName, int maxSpeed, int age, String color, Engine engine) {
+        this.markName = markName;
+        this.maxSpeed = maxSpeed;
+        this.age = age;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Car cloned = (Car) super.clone();
+        cloned.setEngine((Engine) cloned.getEngine().clone());
+        return super.clone();
+    }
+
+    public String getMarkName() {
+        return markName;
+    }
+
+    public void setMarkName(String markName) {
+        this.markName = markName;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "markName='" + markName + '\''
+                + ", maxSpeed=" + maxSpeed
+                + ", age=" + age
+                + ", color='" + color + '\''
+                + ", engine=" + engine
+                + '}';
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,73 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int width;
+    private int length;
+    private int horsePower;
+    private String type;
+    private boolean isBroken;
 
+    public Engine(int width, int length, int horsePower, String type, boolean isBroken) {
+        this.width = width;
+        this.length = length;
+        this.horsePower = horsePower;
+        this.type = type;
+        this.isBroken = isBroken;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isBroken() {
+        return isBroken;
+    }
+
+    public void setBroken(boolean broken) {
+        isBroken = broken;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "width=" + width
+                + ", length=" + length
+                + ", horsePower=" + horsePower
+                + ", type='" + type + '\''
+                + ", isBroken=" + isBroken
+                + '}';
+    }
 }
