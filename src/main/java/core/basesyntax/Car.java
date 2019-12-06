035b263bf3e77596c372adacb916ec6513c5757b
Stanislav Karev <karsv.dvz@gmail.com>
2019-12-06 14:41:07 +0200

    add cloneable to Car and Engine

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int height;
+    private int weight;
+    private String color;
+    private double maxSpeed;
+    private Engine engine;
 
+    public Car(int height, int weight, String color, double maxSpeed, Engine engine) {
+        this.height = height;
+        this.weight = weight;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(this.height, this.weight, this.color, this.maxSpeed, this.engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int ne;
+    private int cylinders;
+    private int mk;
+    private int nd;
+    private int sd;
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
