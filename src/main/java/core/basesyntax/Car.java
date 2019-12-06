c1eabf5da0bd9bebd6f99b4da39ee22d35c79de0
Dmitrii <zinchukdimaa@gmail.com>
2019-12-06 21:28:25 +0200

    added homeWork_5.1_Solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,72 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private String color;
+    private int power;
+    private int maxSpeed;
+    private Engine engine;
 
+    public Car(String brand, String model, String color, int power, int maxSpeed, Engine engine) {
+        this.model = model;
+        this.color = color;
+        this.power = power;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Engine setEngine(Engine engine) {
+        this.engine = engine;
+        return engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand, model, color, power, maxSpeed, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private String model;
+    private String condition;
+    private int power;
+    private int volume;
+
+    public Engine(String type, String model, String condition, int power, int volume) {
+        this.type = type;
+        this.model = model;
+        this.condition = condition;
+        this.power = power;
+        this.volume = volume;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(type, model, condition, power, volume);
+    }
 
 }
