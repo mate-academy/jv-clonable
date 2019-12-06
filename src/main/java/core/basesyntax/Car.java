24beb7584917497877d11ac0a12b3127b5c8fdd8
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-06 15:06:47 +0200

    implemented overriden clone method

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,75 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String model;
+    private int yearOfConstruction;
+    private Engine engine;
+    private int numOfDoors;
+    private String owner;
 
+    public Car(String color, String model, int yearOfConstruction,
+               Engine engine, int numOfDoors, String owner) {
+        this.color = color;
+        this.model = model;
+        this.yearOfConstruction = yearOfConstruction;
+        this.engine = engine;
+        this.numOfDoors = numOfDoors;
+        this.owner = owner;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(int yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getNumOfDoors() {
+        return numOfDoors;
+    }
+
+    public void setNumOfDoors(int numOfDoors) {
+        this.numOfDoors = numOfDoors;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Car cloned = (Car) super.clone();
+        cloned.setEngine((Engine) cloned.getEngine().clone());
+        return cloned;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,83 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int cylinders;
+    private String fuelType;
+    private int yearOfConstruction;
+    private int weight;
+    private int width;
+    private int heights;
 
+    public Engine(String type, int cylinders, String fuelType,
+                  int yearOfConstruction, int weight, int width, int heights) {
+        this.type = type;
+        this.cylinders = cylinders;
+        this.fuelType = fuelType;
+        this.yearOfConstruction = yearOfConstruction;
+        this.weight = weight;
+        this.width = width;
+        this.heights = heights;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public int getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(int yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeights() {
+        return heights;
+    }
+
+    public void setHeights(int heights) {
+        this.heights = heights;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
