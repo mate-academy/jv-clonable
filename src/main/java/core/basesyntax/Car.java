f9a88526ccf1eb13c65213f1299d4764c9df81e9
Daniil Hovorun <hovorun.d@gmail.com>
2019-12-06 19:00:17 +0200

    hw:5.1-clonable solved

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,28 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private Engine engine;
+    private String model;
+    private String color;
+    private String roofType;
+    private int yearOfProduction;
+
+    public Car() {
+    }
+
+    public Car(Engine engine, String model, String color, String roofType, int yearOfProduction) {
+        this.engine = engine;
+        this.model = model;
+        this.color = color;
+        this.roofType = roofType;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car) super.clone();
+        clonedCar.engine = (Engine) clonedCar.engine.clone();
+        return clonedCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int serialNumber;
+    private int cylinderNumber;
+    private int roundsPerMinute;
+    private String model;
+    private String tradeMark;
 
+    public Engine(int serialNumber, int cylinderNumber,
+                  int roundsPerMinute, String model, String tradeMark) {
+        this.serialNumber = serialNumber;
+        this.cylinderNumber = cylinderNumber;
+        this.roundsPerMinute = roundsPerMinute;
+        this.model = model;
+        this.tradeMark = tradeMark;
+    }
+
+    public Engine() {
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
