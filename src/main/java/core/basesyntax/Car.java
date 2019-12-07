c1db6a4954ca52c90f7914783082dc94f145f396
IvanKrivoruchko <krivoruchko.ivan.ukr.net>
2019-12-07 17:15:09 +0200

    Added hw51 solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,31 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String name;
+    private int maxSpeed;
+    private int price;
+    private Engine engine;
 
+    public Car(String color, String name, int maxSpeed, int price, Engine engine) {
+        this.color = color;
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.price = price;
+        this.engine = engine;
+    }
+
+    private Engine getEngine() {
+        return engine;
+    }
+
+    private void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Car clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car) super.clone();
+        clonedCar.setEngine(clonedCar.getEngine().clone());
+        return clonedCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int powerful;
+    private int price;
+    private int weight;
+    private int numberOfCylinders;
+    private String type;
 
+    public Engine(int powerful, int price, int weight,
+                  int numberOfCylinders, String type) {
+        this.powerful = powerful;
+        this.price = price;
+        this.weight = weight;
+        this.numberOfCylinders = numberOfCylinders;
+        this.type = type;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(powerful, price, weight, numberOfCylinders, type);
+    }
 }
