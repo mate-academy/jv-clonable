0a8c85a751395197db69fb540821e35c083b9811
Misha <mishabeheka@gmail.com>
2019-12-08 16:20:17 +0200

    DoneHomeWork_5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,109 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private String modification;
+    private String transmissionType;
+    private Engine engine;
+    private String color;
+
+    public Car(String model, String modification,
+               String transmissionType, Engine engine, String color) {
+        this.model = model;
+        this.modification = modification;
+        this.transmissionType = transmissionType;
+        this.engine = engine;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModification() {
+        return modification;
+    }
+
+    public void setModification(String modification) {
+        this.modification = modification;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(this.getModel(),
+                this.getModification(),
+                this.getTransmissionType(),
+                this.getEngine().clone(),
+                this.getColor());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return model.equals(car.getModel())
+                && modification.equals(car.getModification())
+                && transmissionType.equals(car.getTransmissionType())
+                && engine.equals(car.getEngine())
+                && color.equals(car.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + (model == null ? 0 : model.hashCode());
+        result = prime * result + (modification == null ? 0 : modification.hashCode());
+        result = prime * result + (transmissionType == null ? 0 : transmissionType.hashCode());
+        result = prime * result + (engine == null ? 0 : engine.hashCode());
+        result = prime * result + (color == null ? 0 : color.hashCode());
+        return result;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "model = " + model
+                + " modification = " + modification
+                + " transmissionType = " + transmissionType
+                + " engine = " + engine
+                + " color = " + color
+                + '}';
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,107 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private String typeOfEngine;
+    private int countOfCylinders;
+    private int countOfValves;
+    private double compressionRatio;
+    private String typeOfFuel;
+
+    public Engine(String typeOfEngine, int countOfCylinders,
+                  int countOfValves, double compressionRatio, String typeOfFuel) {
+        this.typeOfEngine = typeOfEngine;
+        this.countOfCylinders = countOfCylinders;
+        this.countOfValves = countOfValves;
+        this.compressionRatio = compressionRatio;
+        this.typeOfFuel = typeOfFuel;
+    }
+
+    public String getTypeOfEngine() {
+        return typeOfEngine;
+    }
+
+    public void setTypeOfEngine(String typeOfEngine) {
+        this.typeOfEngine = typeOfEngine;
+    }
+
+    public int getCountOfCylinders() {
+        return countOfCylinders;
+    }
+
+    public void setCountOfCylinders(int countOfCylinders) {
+        this.countOfCylinders = countOfCylinders;
+    }
+
+    public int getCountOfValves() {
+        return countOfValves;
+    }
+
+    public void setCountOfValves(int countOfValves) {
+        this.countOfValves = countOfValves;
+    }
+
+    public double getCompressionRatio() {
+        return compressionRatio;
+    }
+
+    public void setCompressionRatio(double compressionRatio) {
+        this.compressionRatio = compressionRatio;
+    }
+
+    public String getTypeOfFuel() {
+        return typeOfFuel;
+    }
+
+    public void setTypeOfFuel(String typeOfFuel) {
+        this.typeOfFuel = typeOfFuel;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(this.getTypeOfEngine(),
+                this.getCountOfCylinders(),
+                this.getCountOfValves(),
+                this.getCompressionRatio(),
+                this.getTypeOfFuel());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Engine engine = (Engine) o;
+        return countOfCylinders == engine.countOfCylinders
+                && countOfValves == engine.countOfValves
+                && Double.compare(engine.compressionRatio, compressionRatio) == 0
+                && typeOfEngine.equals(engine.typeOfEngine)
+                && typeOfFuel.equals(engine.typeOfFuel);
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + (typeOfEngine == null ? 0 : typeOfEngine.hashCode());
+        result = prime * result + (countOfCylinders * typeOfEngine.hashCode());
+        result = prime * result + (countOfValves * typeOfFuel.hashCode());
+        long longBits = Double.doubleToLongBits(compressionRatio);
+        result = prime * result + (int) (longBits - (longBits >> 32));
+        result = prime * result + (typeOfFuel == null ? 0 : typeOfFuel.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{ " + "typeOfEngine = " + typeOfEngine
+                + " countOfCylinders = " + countOfCylinders
+                + " countOfValves = " + countOfValves
+                + " compressionRatio = " + compressionRatio
+                + " typeOfFuel = " + typeOfFuel
+                + '}';
+    }
 }
