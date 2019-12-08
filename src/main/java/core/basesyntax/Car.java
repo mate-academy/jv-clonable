3ab4b24b50b8c88350d685f87e14a7b75d230dfb
Taraban <Taraban.dmitry@gmail.com>
2019-12-08 23:48:02 +0200

    added clone-solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,55 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private Engine engine;
+    private int speed;
+    private boolean isWork;
 
+    public Car(String model, String color, Engine engine, int speed, boolean isWork) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+        this.speed = speed;
+        this.isWork = isWork;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isWork() {
+        return isWork;
+    }
+
+    @Override
+    public Object clone() {
+        Car car = null;
+        try {
+            car = (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            car = new Car(
+                    this.model = getModel(),
+                    this.color = getColor(),
+                    this.engine = getEngine(),
+                    this.speed = getSpeed(),
+                    this.isWork = isWork()
+            );
+        }
+        car.engine = (Engine) this.engine.clone();
+        return (Car) clone();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int numberOfCilinders;
+    private int numberOfValves;
+    private String model;
+    private Boolean isWork;
+    private int mileage;
 
+    public Engine(int numberOfCilinders,
+                  int numberOfValves,
+                  String model,
+                  Boolean isWork,
+                  int mileage) {
+        this.numberOfCilinders = numberOfCilinders;
+        this.numberOfValves = numberOfValves;
+        this.model = model;
+        this.isWork = isWork;
+        this.mileage = mileage;
+    }
+
+    public int getNumberOfCilinders() {
+        return numberOfCilinders;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Boolean getWork() {
+        return isWork;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return Engine.super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Engine(this.numberOfCilinders,
+                    this.numberOfValves,
+                    this.model,
+                    this.isWork,
+                    this.mileage);
+        }
+    }
 }
