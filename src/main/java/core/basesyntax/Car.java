af154fc5a528ebeb80d912f82c276324a44af557
Tsehelnyk <tsegelnikoff@gmail.com>
2019-12-08 21:46:32 +0200

    added solution method

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,38 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String brand;
+    private String model;
+    private String color;
+    private String bodyType;
+    private int win;
+    private int yearOfIssue;
+    private Engine engine;
+
+    public Car(String brand, String model, String color, String bodyType,
+               int win, int yearOfIssue) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.bodyType = bodyType;
+        this.win = win;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    @Override
+    public Car clone() {
+        Car clonedCar = new Car(this.brand, this.model, this.color,
+                this.bodyType, this.win, this.yearOfIssue);
+        clonedCar.engine = engine.clone();
+        return clonedCar;
+    }
+
+    //    public static void main(String[] args) {
+    //        Car car = new Car("Subaru", "Forester", "blue", "universal",
+    //                1231245, 2000);
+    //        car.engine = new Engine("opposite", "petrol", 2, 4, 1231654);
+    //        Car clonedCar = car.clone(car);
+    //        System.out.println(clonedCar);
+    //    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+
+    private String type;
+    private String fuel;
+    private int capacity;
+    private int cylinders;
+    private int serialNumber;
+
+    public Engine(String type, String fuel, int capacity, int cylinders, int serialNumber) {
+        this.type = type;
+        this.fuel = fuel;
+        this.capacity = capacity;
+        this.cylinders = cylinders;
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    public Engine clone() {
+        Engine clonedEngine = new Engine(this.type, this.fuel, this.capacity,
+                this.cylinders, this.serialNumber);
+        return clonedEngine;
+    }
 
 }
