e111e3ddbca79e119a8ea429f4d40439130a9bc0
Serhii Blakh <s.a.blakh@ukr.net>
2019-12-08 23:04:32 +0200

    Added solution homework_5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String type;
+    private String model;
+    private int maxSpeed;
+    private boolean isFaster;
+    private Engine engine;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public boolean isFaster() {
+        return isFaster;
+    }
+
+    public void setFaster(boolean faster) {
+        isFaster = faster;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Car clone() throws CloneNotSupportedException {
+        return (Car) super.clone();
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int countOfCylinder;
+    private int countOfValve;
+    private int power;
+    private boolean isElectric;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCountOfCylinder() {
+        return countOfCylinder;
+    }
+
+    public void setCountOfCylinder(int countOfCylinder) {
+        this.countOfCylinder = countOfCylinder;
+    }
+
+    public int getCountOfValve() {
+        return countOfValve;
+    }
+
+    public void setCountOfValve(int countOfValve) {
+        this.countOfValve = countOfValve;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public boolean getIsElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        this.isElectric = electric;
+    }
+
+    public Engine(String type, int countOfCylinder, int countOfValve,
+                  int power, boolean isElectric) {
+        this.type = type;
+        this.countOfCylinder = countOfCylinder;
+        this.countOfValve = countOfValve;
+        this.power = power;
+        this.isElectric = isElectric;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
