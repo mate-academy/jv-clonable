ad29440744a9fa52d88a261a27f63f8a3f918123
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-09 00:53:40 +0200

    5.1 is done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String color;
+    private String model;
+    private int maxSpeed;
+    private int cost;
+
+    public Car(Engine engine, String color, String model, int maxSpeed, int cost) {
+        this.engine = engine;
+        this.color = color;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.cost = cost;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(engine.clone(), color, model, maxSpeed, cost);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private int serialNumber;
+    private String brand;
+    private boolean available;
+    private int size;
 
+    public Engine(int power, int serialNumber, String brand, boolean available, int size) {
+        this.power = power;
+        this.serialNumber = serialNumber;
+        this.brand = brand;
+        this.available = available;
+        this.size = size;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(power, serialNumber, brand, available, size);
+    }
 }
