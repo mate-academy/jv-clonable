5c9163dd79e82529103d33a40c3f5a23d6497f06
karsv <karsv.dvz@gmail.com>
2019-12-09 11:16:47 +0200

    add chenges engine

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,14 +1,22 @@
 package core.basesyntax;
 
 public class Engine implements Cloneable {
-    private int ne;
-    private int cylinders;
-    private int mk;
-    private int nd;
+    private int power;
+    private String cylinders;
+    private int torqueMoment;
+    private int rotationSpeed;
     private int sd;
+
+    public Engine(int power, String cylinders, int torqueMoment, int rotationSpeed, int sd) {
+        this.power = power;
+        this.cylinders = cylinders;
+        this.torqueMoment = torqueMoment;
+        this.rotationSpeed = rotationSpeed;
+        this.sd = sd;
+    }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+        return new Engine(power, cylinders, torqueMoment, rotationSpeed, sd);
     }
 }
