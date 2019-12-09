c7e0928c642534dbb8a2e015ac744269a4716258
levko <levkosvd@gmail.com>
2019-12-09 16:19:27 +0200

    hm5.1 v1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private Engine engine;
+    private String color;
+    private String brand;
+    private int price;
+    private int maxSpeed;
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Car(Engine engine, String color, String brand, int price, int maxSpeed) {
+        this.engine = engine;
+        this.color = color;
+        this.brand = brand;
+        this.price = price;
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new Car(engine,color,brand,price,maxSpeed);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private String serialNumber;
+    private int price;
+    private int volume;
+    private int power;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public Engine(String type, String serialNumber, int price, int volume, int power) {
+        this.type = type;
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.volume = volume;
+        this.power = power;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(type,serialNumber,price,volume,power);
+    }
 }
