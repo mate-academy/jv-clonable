86e7d278290b2dd65f03f2359523d135f18999bf
Vadim Chaika <proxik777@gmail.com>
2019-12-09 16:17:03 +0200

    added a solution for car/engine hw

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private String color;
+    private int maxSpeed;
+    private int year;
+    private Engine engine;
+
+    public Car(String model, String color, int maxSpeed, int year, Engine engine) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public Engine engine() {
+        return engine;
+    }
+
+    public Car clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car)super.clone();
+        return clonedCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,40 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private int engineSpeeds;
+    private int enginePower;
+    private String engineType;
+    private String engineYear;
+    private int torque;
+
+    public int getEngineSpeeds() {
+        return engineSpeeds;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getEngineYear() {
+        return engineYear;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public Engine(int engineSpeeds, int enginePower, int torque,
+                  String engineType, String engineYear) {
+        this.engineSpeeds = engineSpeeds;
+        this.enginePower = enginePower;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
