2b8fa30ab6a580ccef9af6da95ad780c6c3376d3
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-09 21:17:08 +0200

    task done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements  Cloneable {
 
+    private int quantityOfWheels;
+    private String color;
+    private boolean leftSteeringWheel;
+    private Engine engine;
+
+    public int getQuantityOfWheels() {
+        return quantityOfWheels;
+    }
+
+    public void setQuantityOfWheels(int quantityOfWheels) {
+        this.quantityOfWheels = quantityOfWheels;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isLeftSteeringWheel() {
+        return leftSteeringWheel;
+    }
+
+    public void setLeftSteeringWheel(boolean leftSteeringWheel) {
+        this.leftSteeringWheel = leftSteeringWheel;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public boolean isTransmissionAuto() {
+        return transmissionAuto;
+    }
+
+    public void setTransmissionAuto(boolean transmissionAuto) {
+        this.transmissionAuto = transmissionAuto;
+    }
+
+    private boolean transmissionAuto;
+
+    public Car(int quantityOfWheels, String color, boolean leftSteeringWheel, Engine engine) {
+        this.quantityOfWheels = quantityOfWheels;
+        this.color = color;
+        this.leftSteeringWheel = leftSteeringWheel;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(quantityOfWheels, color, leftSteeringWheel, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean oilEngine;
+    private int weight;
+    private int width;
+    private int length;
+    private String label;
 
+    private Engine(boolean oilEngine, int weight, int width, int length, String label) {
+        this.oilEngine = oilEngine;
+        this.weight = weight;
+        this.width = width;
+        this.length = length;
+        this.label = label;
+    }
+
+    public void setOilEngine(boolean oilEngine) {
+        this.oilEngine = oilEngine;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(oilEngine, weight, width, length, label);
+    }
 }
