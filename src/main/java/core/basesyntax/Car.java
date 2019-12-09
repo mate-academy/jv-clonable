ab773b052b4c14fc953523e26a649ff5640fdd01
IegorSoboliev <evsobolev@gmail.com>
2019-12-09 14:30:33 +0200

    Done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int year;
+    private String colour;
+    private String model;
+    private int speed;
+    private Engine engine;
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Car(int year, String colour, String model, int speed, Engine engine) {
+        this.year = year;
+        this.colour = colour;
+        this.model = model;
+        this.speed = speed;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(getYear(), getColour(), getModel(), getSpeed(), getEngine());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String brand;
+    private int capacity;
+    private String type;
+    private String fuelType;
+    private int efficiency;
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public void setEfficiency(int efficiency) {
+        this.efficiency = efficiency;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public int getEfficiency() {
+        return efficiency;
+    }
+
+    public Engine(String brand, int capacity, String type, String fuelType, int efficiency) {
+        this.brand = brand;
+        this.capacity = capacity;
+        this.type = type;
+        this.fuelType = fuelType;
+        this.efficiency = efficiency;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Engine(getBrand(), getCapacity(), getType(), getFuelType(), getEfficiency());
+    }
 }
