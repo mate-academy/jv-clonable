6644035d0123f0bdf1396c75546e480bd2f5eed6
Alexandr Sashchenko <santys87@gmail.com>
2019-12-09 15:27:07 +0200

    Added mate HW5.1 Solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,74 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String colour;
+    private int seats;
+    private int doors;
+    private Engine engine;
 
+    public Car(
+            String model,
+            String colour,
+            int seats,
+            int doors,
+            Engine engine
+    ) {
+        this.model = model;
+        this.colour = colour;
+        this.seats = seats;
+        this.doors = doors;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car car = (Car) super.clone();
+        Engine engine = this.getEngine().clone();
+        car.setEngine(engine);
+        return car;
+    }
 }
+
+
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private int torque;
+    private int weight;
+    private int cylinders;
+    private String cylindersPosition;
 
+    public Engine(
+            int power,
+            int torque,
+            int weight,
+            int cylinders,
+            String cylindersPosition
+    ) {
+        this.power = power;
+        this.torque = torque;
+        this.weight = weight;
+        this.cylinders = cylinders;
+        this.cylindersPosition = cylindersPosition;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getCylindersPosition() {
+        return cylindersPosition;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public void setCylindersPosition(String cylindersPosition) {
+        this.cylindersPosition = cylindersPosition;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
