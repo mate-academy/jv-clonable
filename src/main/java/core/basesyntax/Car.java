a4b9525740d968bae1e3f92cbabedba4e7b4c612
Yaroslav <yaroslav2875@i.ua>
2019-12-09 12:35:24 +0200

    added hw 5.1 solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private int speed;
+    private Engine engine;
+    private int age;
+    private String color;
 
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        Car car = (Car)super.clone();
+        car.engine = engine.clone();
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private double power;
+    private double price;
+    private int weight;
+    private boolean type;
 
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return (Engine)super.clone();
+    }
 }
