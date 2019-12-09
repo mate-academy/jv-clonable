8a773461902de7d55aa346c6a0f43fa164bbe2de
Riptod <otoval15>
2019-12-09 13:34:43 +0200

    fixed solution

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -7,10 +7,6 @@ public class Engine implements Cloneable {
     private double weight;
     private int power;
 
-    public Engine() {
-
-    }
-
     public Engine(String name, String fuel, double capacity, double weight, int power) {
         this.name = name;
         this.fuel = fuel;
@@ -19,54 +15,8 @@ public class Engine implements Cloneable {
         this.power = power;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFuel() {
-        return fuel;
-    }
-
-    public void setFuel(String fuel) {
-        this.fuel = fuel;
-    }
-
-    public double getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(double capacity) {
-        this.capacity = capacity;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public int getPower() {
-        return power;
-    }
-
-    public void setPower(int power) {
-        this.power = power;
-    }
-
     @Override
-    public Engine clone() throws CloneNotSupportedException {
-        Engine engine = new Engine();
-        engine.setName(this.name);
-        engine.setFuel(this.fuel);
-        engine.setCapacity(this.capacity);
-        engine.setWeight(this.weight);
-        engine.setPower(this.power);
-        return engine;
+    public Engine clone() {
+        return new Engine(name, fuel, capacity, weight, power);
     }
 }
