26cc1d2fd653e8bb6c6f3cafeb8df1f6c8ba3bf8
woododoCode <woododoCode@users.noreply.github.com>
2020-04-02 16:08:32 +0300

    Solution hw41

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String year;
+    private String make;
+    private String model;
+    private String color;
+    private int doorsQuantity;
+    private Engine engine;
+
+    public Car(String year, String make, String model, String color,
+               int doorsQuantity, Engine engine) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.doorsQuantity = doorsQuantity;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(year, make, model, color, doorsQuantity, (Engine) engine.clone());
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String basicModel;
+    private int fuelType;
+    private int numberOfCylinder;
+    private String compressionRatio;
+    private double maxPower;
 
+    public Engine(String basicModel, int fuelType, int numberOfCylinder,
+                  String compressionRatio, double maxPower) {
+        this.basicModel = basicModel;
+        this.fuelType = fuelType;
+        this.numberOfCylinder = numberOfCylinder;
+        this.compressionRatio = compressionRatio;
+        this.maxPower = maxPower;
+    }
+
+    @Override
+    public Object clone() {
+        return new Engine(basicModel, fuelType, numberOfCylinder, compressionRatio, maxPower);
+    }
 }
