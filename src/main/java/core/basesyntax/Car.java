cd075d9102caabdabd12d51b38c1183cf2e8a588
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-04-02 22:35:37 +0300

    hw 5.1 solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,64 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String brand;
+    private String model;
+    private int price;
+    private int weight;
+    private String colour;
+    private Engine engine = new Engine();
+
+    public Car(String brand, String model, int price, int weight, String colour) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.weight = weight;
+        this.colour = colour;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel(String model) {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,66 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private String type;
+    private double capacity;
+    private double weight;
+    private String state;
+    private int price;
+
+    public Engine() {
+    }
+
+    public Engine(String type, String state, int price, double capacity, double wight) {
+        this.type = type;
+        this.state = state;
+        this.price = price;
+        this.capacity = capacity;
+        this.weight = wight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
