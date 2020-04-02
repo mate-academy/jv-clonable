320ce854347277e28deba358bc30c780d92077cd
Nick97-git <nickarchangel97@gmail.com>
2020-04-02 15:48:12 +0300

    fixed method clone() in Engine class

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -8,8 +8,17 @@ public class Engine implements Cloneable {
     private String typeOfFuel;
     private String typeOfCooling;
 
+    public Engine(int levelOfNoise, int numOfCylinders, int power,
+                  String typeOfFuel, String typeOfCooling) {
+        this.levelOfNoise = levelOfNoise;
+        this.numOfCylinders = numOfCylinders;
+        this.power = power;
+        this.typeOfFuel = typeOfFuel;
+        this.typeOfCooling = typeOfCooling;
+    }
+
     @Override
     public Engine clone() {
-        return new Engine();
+        return new Engine(levelOfNoise,numOfCylinders,power,typeOfFuel,typeOfCooling);
     }
 }
