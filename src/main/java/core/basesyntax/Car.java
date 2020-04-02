f26ef6237d422e4eb63c16bcbb4db662385bf8fb
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-02 14:40:59 +0300

    added solution of hw-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String model;
+    private boolean hasAutomaticTransmission;
+    private Engine engine;
+    private int mileage;
 
+    public Car(String color, String model, boolean hasAutomaticTransmission, Engine engine,
+               int mileage) {
+        this.color = color;
+        this.model = model;
+        this.hasAutomaticTransmission = hasAutomaticTransmission;
+        this.engine = engine;
+        this.mileage = mileage;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(color, model, hasAutomaticTransmission, engine.clone(), mileage);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int volume;
+    private String model;
+    private String fuelType;
+    private int power;
+    private int fuelConsumption;
 
+    public Engine(int volume, String model, String fuelType, int power, int fuelConsumption) {
+        this.volume = volume;
+        this.model = model;
+        this.fuelType = fuelType;
+        this.power = power;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(volume, model, fuelType, power, fuelConsumption);
+    }
 }
