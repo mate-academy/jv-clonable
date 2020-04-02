e77e088d47e1db1fbe7e31b6f771333346495ec9
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-04-02 15:15:59 +0300

    implemented Engine and Car classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,55 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String mark;
+    private String model;
+    private String typeOfBody;
+    private int maxSpeed;
+    private Engine engine;
 
+    public Car(String mark, String model, String typeOfBody, int maxSpeed, Engine engine) {
+        this.mark = mark;
+        this.model = model;
+        this.typeOfBody = typeOfBody;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getTypeOfBody() {
+        return typeOfBody;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car) super.clone();
+        return new Car(newCar.getMark(),
+                newCar.getModel(),
+                newCar.getTypeOfBody(),
+                newCar.maxSpeed,
+                newCar.engine);
+    }
 }
