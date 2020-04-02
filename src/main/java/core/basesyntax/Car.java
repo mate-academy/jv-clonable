1c22bd88c2761ea2dd8135705849182713a01154
NastiaFasova <fasovaaa@fmail.com>
2020-04-02 13:46:07 +0300

    Added solution to the 1-st task of HW-4

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String color;
+    private String carBrand;
+    private String fuelType;
+    private String bodyType;
 
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        Car car = (Car)super.clone();
+        engine = car.getEngine() ;
+        engine = new Engine();
+        return car;
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double cylinderDiameter;
+    private double pistonStroke;
+    private double volume;
+    private double crankDiameter;
+    private double capacity;
 
+    public Object clone() throws CloneNotSupportedException {
+        Engine engine = (Engine)super.clone();
+        return engine;
+    }
 }
