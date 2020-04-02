80b8ed66ebe4d09a7ee43c92fac0fa604fb230f8
Богдан Демьянчук <t0974447144@gmail.com>
2020-04-02 13:37:08 +0300

    Solution HW 5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String mark;
+    private String type;
+    private int maxSpeed;
+    private String color;
+    private Engine nameEngine;
+
+    @Override
+    public Car clone() {
+        Car clonedCar = new Car();
+        clonedCar.mark = this.mark;
+        clonedCar.type = this.type;
+        clonedCar.maxSpeed = this.maxSpeed;
+        clonedCar.color = this.color;
+        clonedCar.nameEngine = this.nameEngine;
+        return clonedCar;
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int capacity;
+    private String typeFuel;
+    private boolean turbine;
+    private double cylinderBore;
+    private double torque;
 
+    @Override
+    public Engine clone() {
+        Engine clonedEngine = new Engine();
+        clonedEngine.capacity = this.capacity;
+        clonedEngine.typeFuel = this.typeFuel;
+        clonedEngine.turbine = this.turbine;
+        clonedEngine.cylinderBore = this.cylinderBore;
+        clonedEngine.torque = this.torque;
+        return clonedEngine;
+    }
 }
