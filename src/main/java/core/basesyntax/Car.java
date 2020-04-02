38afa05597cea8f6116ad9b60f97ee5f2475e17c
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-04-02 12:38:39 +0300

    add solution for clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private String color;
+    private double value;
+    private Engine engine;
 
+    public Car(String bd, String ml, String clr, double val, Engine eng) {
+        brand = bd;
+        model = ml;
+        color = clr;
+        value = val;
+        engine = eng;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand, model, color, value, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,14 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private String typeEngine;
+    private double engineCapacity;
+    private int horsePowers;
+    private int weight;
 
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine();
+    }
 }
