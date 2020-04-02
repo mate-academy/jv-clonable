44d35a3deb0530af22014ed89c4bf661f9e549e9
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-02 13:08:04 +0300

    added method clone()

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String colour;
+    private String name;
+    private String type;
+    private int yearOfIssue;
 
+    public Car(Engine engine, String colour, String name, String type, int yearOfIssue) {
+        this.engine = engine;
+        this.colour = colour;
+        this.name = name;
+        this.type = type;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(engine.clone(), colour, name, type, yearOfIssue);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String series;
+    private int displacing;
+    private int size;
+    private String typeOfEnergy;
+    private String forOrganizing;
 
+    public Engine(String series, int displacing, int size,
+                  String typeOfEnergy, String forOrganizing) {
+        this.series = series;
+        this.displacing = displacing;
+        this.size = size;
+        this.typeOfEnergy = typeOfEnergy;
+        this.forOrganizing = forOrganizing;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return new Engine(series, displacing, size, typeOfEnergy, forOrganizing);
+    }
 }
