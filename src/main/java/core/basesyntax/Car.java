72140fd4eb5c32ad9a4092add6eb47d5e9125367
ihor <krohmal.igor777@gmail.com>
2020-04-02 17:05:48 +0300

    implementation classes Car and Engine

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private String color;
+    private int weight;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String model, String color, int weight, int maxSpeed, Engine engine) {
+        this.model = model;
+        this.color = color;
+        this.weight = weight;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(model, color, weight, maxSpeed, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String fuel;
+    private int capacity;
+    private int weight;
+    private int numberOfCylinders;
+    private String modification;
 
+    public Engine(String fuel, int capacity, int weight,
+                  int numberOfCylinders, String modification) {
+        this.fuel = fuel;
+        this.capacity = capacity;
+        this.weight = weight;
+        this.numberOfCylinders = numberOfCylinders;
+        this.modification = modification;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public String getModification() {
+        return modification;
+    }
+
+    public void setModification(String modification) {
+        this.modification = modification;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(fuel, capacity, weight, numberOfCylinders, modification);
+    }
 }
