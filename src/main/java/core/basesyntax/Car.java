68bbdf3cac2bdd85e51b5687237c7e10f5479658
ana-dav <opa.super.op@gmail.com>
2020-04-02 13:50:53 +0300

    5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,44 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable{
+    private String brand;
+    private String color;
+    private int speed;
+    private int doorsNum;
+    private Engine carEngine;
 
+    public Car(String brand, String color, int speed, int doorsNum, Engine carEngine) {
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+        this.doorsNum = doorsNum;
+        this.carEngine = carEngine;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDoorsNum() {
+        return doorsNum;
+    }
+
+    public Engine getCarEngine() {
+        return carEngine;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Engine engine= (Engine) super.clone();
+        engine = (Engine) this.carEngine.clone();
+        return engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable{
+    private int power;
+    private int torque;
+    private int diameter;
+    private String model;
+    private int serialNumber;
 
+    public Engine(Engine engine) {
+        this.power = engine.power;
+        this.torque = engine.torque;
+        this.diameter = engine.diameter;
+        this.model = engine.model;
+        this.serialNumber = engine.serialNumber;
+    }
+
+    public Engine(int power, int torque, int diameter, String model, int serialNumber) {
+        this.power = power;
+        this.torque = torque;
+        this.diameter = diameter;
+        this.model = model;
+        this.serialNumber = serialNumber;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
