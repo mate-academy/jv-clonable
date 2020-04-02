fffd13aae046801e5e41869cb949312cf764607d
Filipp <filipp888@gmail.com>
2020-04-02 13:27:40 +0200

    Added solution for Task 5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,87 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private int maxSpeed;
+    private int capacity;
+    private Engine engine;
 
+    public Car(String name, String color, int maxSpeed, int capacity, Engine engine) {
+        this.name = name;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.capacity = capacity;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "name='" + name
+                + '\''
+                + ", color='" + color
+                + '\''
+                + ", maxSpeed=" + maxSpeed
+                + ", capacity=" + capacity
+                + ", engine=" + engine
+                + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car car = null;
+        try {
+            car = (Car) super.clone();
+        } catch (CloneNotSupportedException exception) {
+            car = new Car(
+                    this.name,
+                    this.getColor(),
+                    this.maxSpeed,
+                    this.capacity,
+                    this.engine
+            );
+        }
+        car.engine = (Engine) this.engine.clone();
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,88 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String engineName;
+    private String engineType;
+    private int cylindres;
+    private int power;
+    private int fuelTankCapacity;
 
+    public Engine(String engineName,
+                  String engineType,
+                  int cylindres,
+                  int power,
+                  int fuelTankCapacity) {
+        this.engineName = engineName;
+        this.engineType = engineType;
+        this.cylindres = cylindres;
+        this.power = power;
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "engineName='" + engineName
+                + '\''
+                + ", engineType='" + engineType
+                + '\''
+                + ", cylindres=" + cylindres
+                + ", power=" + power
+                + ", fuelTankCapacity=" + fuelTankCapacity
+                + '}';
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException exception) {
+            return new Engine(
+                    this.engineName,
+                    this.engineType,
+                    this.cylindres,
+                    this.power,
+                    this.fuelTankCapacity);
+        }
+    }
+
+    public String getEngineName() {
+        return engineName;
+    }
+
+    public void setEngineName(String engineName) {
+        this.engineName = engineName;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getCylindres() {
+        return cylindres;
+    }
+
+    public void setCylindres(int cylindres) {
+        this.cylindres = cylindres;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getFuelTankCapacity() {
+        return fuelTankCapacity;
+    }
+
+    public void setFuelTankCapacity(int fuelTankCapacity) {
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
 }
