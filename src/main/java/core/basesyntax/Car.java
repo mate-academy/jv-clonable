4f4d865465b136e562912852d20db6e5eaa0b743
petrukillya <petrukillya98@gmail.com>
2020-04-02 17:15:44 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int seatingCapacity;
+    private String typeWheelDrive;
+    private String transmissionType;
+    private String bodyType;
+    private Engine engine;
 
+    public Car(int seatingCapacity, String typeWheelDrive, String transmissionType,
+               String bodyType, Engine engine) {
+        this.seatingCapacity = seatingCapacity;
+        this.typeWheelDrive = typeWheelDrive;
+        this.transmissionType = transmissionType;
+        this.bodyType = bodyType;
+        this.engine = engine;
+    }
+
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(seatingCapacity, typeWheelDrive, transmissionType,bodyType,engine.clone());
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String fuelType;
+    private double fuelConsumption;
+    private int maxPower;
+    private int maxRpm;
+    private int numeralOfCylinder;
 
+    public Engine(String fuelType, double fuelConsumption, int maxPower,
+                  int maxRpm, int numeralOfCylinder) {
+        this.fuelType = fuelType;
+        this.fuelConsumption = fuelConsumption;
+        this.maxPower = maxPower;
+        this.maxRpm = maxRpm;
+        this.numeralOfCylinder = numeralOfCylinder;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(fuelType, fuelConsumption, maxPower, maxRpm, numeralOfCylinder);
+    }
 }
