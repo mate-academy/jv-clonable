e49b572702e05b0010a6afbaafac29a63acb91c5
Ludchenko <skillet367@icloud.com>
2020-04-02 20:44:07 +0300

    added all required fields and methods

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String model;
+    private int speed;
+    private boolean isRunning;
+    private core.basesyntax.Engine engine;
 
+    Car(String color, String model, int speed, boolean isRunning, core.basesyntax.Engine engine) {
+        this.color = color;
+        this.model = model;
+        this.speed = speed;
+        this.isRunning = isRunning;
+        this.engine = engine;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public boolean getRunning() {
+        return isRunning;
+    }
+
+    public void setEngine(core.basesyntax.Engine engine) {
+        this.engine = engine;
+    }
+
+    public core.basesyntax.Engine getEngine() {
+        return engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(color, model, speed, isRunning, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,46 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean isOn;
+    private float size;
+    private String type;
+    private String model;
+    private String dateOfCreation;
 
+    Engine(boolean isOn, float size, String type, String model, String dateOfCreation) {
+        this.isOn = isOn;
+        this.size = size;
+        this.type = type;
+        this.model = model;
+        this.dateOfCreation = dateOfCreation;
+    }
+
+    public void setIsOn(boolean isOn) {
+        this.isOn = isOn;
+    }
+
+    public boolean getIsOn() {
+        return isOn;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(isOn, size, type, model, dateOfCreation);
+    }
 }
