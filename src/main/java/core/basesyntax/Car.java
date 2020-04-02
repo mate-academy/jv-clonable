df3299ddc25c49c9b8747025ec22fc40953c31f2
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-04-02 22:32:13 +0300

    Clone created

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String colour;
+    private String type;
+    private int maxSpeed;
+    private int acceleration;
+    private Engine engine;
 
+    public Car(String name, String colour, String type,
+               int maxSpeed, int fuelType, Engine engine) {
+        this.name = name;
+        this.colour = colour;
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+        this.engine = engine;
+    }
+
+    @Override
+
+    public Car clone() {
+        return new Car(name, colour, type, maxSpeed, acceleration, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private String model;
+    private String type;
+    private String maxPower;
+    private String averagePower;
 
+    public Engine(String name, String model, String type,
+                  String maxPower, String averagePower) {
+        this.name = name;
+        this.model = model;
+        this.type = type;
+        this.maxPower = maxPower;
+        this.averagePower = averagePower;
+    }
+
+    @Override
+
+    public Engine clone() {
+        return new Engine(name, model, type, maxPower, averagePower);
+    }
 }
