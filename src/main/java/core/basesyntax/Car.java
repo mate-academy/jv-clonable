84b0f34a1766fc6b28b095c03e9534e470722b76
Galina <gala6ap@mail.ru>
2020-04-02 12:44:43 +0300

    Car and Engine classes implementation added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String producer;
+    private String model;
+    private String color;
+    private int productionYear;
+    private Engine engine;
 
+    public Car(String producerName, String modelName, String colorName,
+               int year, Engine carEngine) {
+        producer = producerName;
+        model = modelName;
+        color = colorName;
+        productionYear = year;
+        engine = carEngine;
+    }
+
+    public Car clone() {
+        return new Car(producer, model, color, productionYear, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private String producer;
+    private String model;
+    private int voltage;
+    private int power;
 
+    public Engine(String typeName, String producerName, String modelName,
+                  int voltageValue, int powerValue) {
+        type = typeName;
+        producer = producerName;
+        model = modelName;
+        voltage = voltageValue;
+        power = powerValue;
+    }
+
+    public Engine clone() {
+        return new Engine(type, producer, model, voltage, power);
+    }
 }
