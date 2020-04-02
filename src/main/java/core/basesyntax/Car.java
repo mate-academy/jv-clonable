5caf7b13a6671f92e2fc5095032e370c84adb5e2
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-02 14:50:05 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String color;
+    private int maxSpeed;
+    private int doors;
+    private boolean isNew;
 
+    public Car(Engine engine, String color, int maxSpeed, int doors, boolean isNew) {
+        this.isNew = isNew;
+        this.doors = doors;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.isNew = isNew;
+    }
+
+    public Car clone(Car other) throws CloneNotSupportedException {
+        return new Car(other.engine, other.color, other.maxSpeed, other.doors, other.isNew);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String typeOfFuel;
+    private int power;
+    private int volume;
+    private int price;
+    private boolean isNew;
 
+    public Engine(String typeOfFuel, int power, int volume, int price, boolean isNew) {
+        this.typeOfFuel = typeOfFuel;
+        this.power = power;
+        this.volume = volume;
+        this.price = price;
+        this.isNew = isNew;
+    }
+
+    public Engine clone(Engine other) throws CloneNotSupportedException {
+        return new Engine(other.typeOfFuel,
+                other.power, other.volume, other.price, other.isNew);
+    }
 }
