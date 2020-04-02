860e28aeb9d810b9fb126483cd7a2db927c48fb5
Володимир <matrix.vova@gmail.com>
2020-04-02 17:24:57 +0300

    renamed variable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -6,18 +6,18 @@ public class Car implements Cloneable {
     private String name;
     private int year;
     private int speed;
-    private Engine engine1;
+    private Engine engine;
 
     public Car(String colour, String name, int year, int speed, Engine engine1) {
         this.colour = colour;
         this.name = name;
         this.year = year;
         this.speed = speed;
-        this.engine1 = engine1;
+        this.engine = engine;
     }
 
     @Override
     public Car clone() throws CloneNotSupportedException {
-        return new Car(colour, name, year, speed, engine1.clone());
+        return new Car(colour, name, year, speed, engine.clone());
     }
 }
