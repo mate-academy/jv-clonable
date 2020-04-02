58a83bf4575aba0ec8f293ba799619445478fee6
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-02 16:16:36 +0300

    added solution (internal commit)

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int mileage;
+    private long serialNum;
+    private String vendorName;
+    private String model;
+    private Engine engine;
 
+    public Car(int mileage, long serialNum, String vendorName, String model, Engine engine) {
+        this.mileage = mileage;
+        this.model = model;
+        this.serialNum = serialNum;
+        this.vendorName = vendorName;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(mileage, serialNum, vendorName, model, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String vendor;
+    private int sizeX;
+    private int sizeY;
+    private int sizeZ;
 
+    public Engine(String model, String vendor, int sizeX, int sizeY, int sizeZ) {
+        this.model = model;
+        this.vendor = vendor;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.sizeZ = sizeZ;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(model, vendor, sizeX, sizeY, sizeZ);
+    }
 }
