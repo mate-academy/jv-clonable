1ac21344449191c9895ebbce02a2f4925efc7917
Serhii Maksymov <serhio.maksymov@gmail.com>
2020-04-02 19:27:45 +0300

    set all fields with constructor

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -7,6 +7,22 @@ public class Car implements Cloneable {
     private int weight;
     private Engine engine;
 
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public void setCarSpeed(int carSpeed) {
+        this.carSpeed = carSpeed;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
     public void setEngine(Engine engine) {
         this.engine = engine;
     }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -7,6 +7,26 @@ public class Engine implements Cloneable {
     private int enginePower;
     private int engineTorque;
 
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public void setCylindersNumber(int cylindersNumber) {
+        this.cylindersNumber = cylindersNumber;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public void setEngineTorque(int engineTorque) {
+        this.engineTorque = engineTorque;
+    }
+
     @Override
     protected Engine clone() {
         return new Engine();
