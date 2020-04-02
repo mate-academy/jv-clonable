bd0691ef9a84194f905e7ed6413a340eb617c732
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-04-02 15:54:08 +0300

    added solution to hw-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private Engine ownEngine;
+    private String color;
+    private String transmission;
+    private int speed;
+
+    public Car(String model, Engine ownEngine, String color, String transmission, int speed) {
+        this.model = model;
+        this.ownEngine = ownEngine;
+        this.color = color;
+        this.transmission = transmission;
+        this.speed = speed;
+    }
+
+    @Override
+
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(model, ownEngine, color, transmission, speed);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private int volume;
+    private String type;
+    private boolean turbo;
+    private int amountTurnines;
+    private String location;
+
+    public Engine(int volume, String type, boolean turbo, int amountTurnines, String location) {
+        this.volume = volume;
+        this.type = type;
+        this.turbo = turbo;
+        this.amountTurnines = amountTurnines;
+        this.location = location;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return new Engine(volume, type, turbo, amountTurnines, location);
+    }
 }
