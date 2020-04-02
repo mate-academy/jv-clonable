5de9ff846cc10d22cf72c75e76ee922e2f05ea81
Andrew <tolstuy1996@gmail.com>
2020-04-02 21:58:21 +0300

    done clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private String kind;
+    private int price;
+    private Engine engine;
 
+    public Car(String name, String color, String kind, int price, Engine engine) {
+        this.name = name;
+        this.color = color;
+        this.kind = kind;
+        this.price = price;
+        this.engine = engine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(name, color, kind, price, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double power;
+    private double capacity;
+    private double fuel;
+    private double oil;
+    private int idNumber;
 
+    public Engine(double power, double capacity, double fuel, double oil, int idNumber) {
+        this.power = power;
+        this.capacity = capacity;
+        this.fuel = fuel;
+        this.oil = oil;
+        this.idNumber = idNumber;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public double getOil() {
+        return oil;
+    }
+
+    public void setOil(double oil) {
+        this.oil = oil;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(power, capacity, fuel, oil, idNumber);
+    }
 }
