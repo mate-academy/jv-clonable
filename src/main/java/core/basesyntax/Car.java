123d66065a6fe7e4a3047a1a0313002f2fcb2260
Djeckkenni <djeck27kenni@gmail.com>
2020-04-02 17:32:26 +0300

    correct code

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,78 @@
 package core.basesyntax;
 
-public class Car {
+import java.awt.Color;
 
+public class Car implements Cloneable {
+    private String carMake;
+    private String modelOfCar;
+    private Color colorCar;
+    private int yearOfIssue;
+    private boolean airBag;
+    private Engine engine;
+
+    public Car(String carMake, String modelOfCar, Color colorCar,int yearOfIssue,boolean airBag,
+                Engine engine) {
+        this.carMake = carMake;
+        this.modelOfCar = modelOfCar;
+        this.colorCar = colorCar;
+        this.yearOfIssue = yearOfIssue;
+        this.airBag = airBag;
+        this.engine = engine;
+    }
+
+    public void setCarMake(String carMake) {
+        this.carMake = carMake;
+    }
+
+    public void setModelOfCar(String modelOfCar) {
+        this.modelOfCar = modelOfCar;
+    }
+
+    public void setColorCar(Color colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public void setYearOfIssue(int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public void setAirBag(boolean airBag) {
+        this.airBag = airBag;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getCarMake() {
+        return carMake;
+    }
+
+    public String getModelOfCar() {
+        return modelOfCar;
+    }
+
+    public Color getColorCar() {
+        return colorCar;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public boolean getAirBag() {
+        return airBag;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Car cloned = (Car) super.clone();
+        cloned.setEngine((Engine)cloned.getEngine().clone());
+        //cloned.setDepartment((Department)cloned.getDepartment().clone());
+        return cloned;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private int volume;
+    private int power;
+    private int accelerationTime;
+    private int torque;
 
+    public Engine(String name, int volume, int power, int accelerationTime, int torque) {
+        this.name = name;
+        this.volume = volume;
+        this.power = power;
+        this.accelerationTime = accelerationTime;
+        this.torque = torque;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setAccelerationTime(int accelerationTime) {
+        this.accelerationTime = accelerationTime;
+    }
+
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getAccelerationTime() {
+        return accelerationTime;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
