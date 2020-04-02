fc5d353caec6922de00671199e0d753797927c20
vladyslav <vlad.bes0711@gmail.com>
2020-04-02 12:44:19 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private int numOfSits;
+    private Engine engine;
+    private String type;
+    private int numOfWheels;
 
+    public Car(String model, int numOfSits, Engine engine, String type, int numOfWheels) {
+        this.model = model;
+        this.numOfSits = numOfSits;
+        this.engine = engine;
+        this.type = type;
+        this.numOfWheels = numOfWheels;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(model, numOfSits, engine, type, numOfWheels);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String capacity;
+    private String mark;
+    private String power;
+    private String type;
+    private boolean isRun;
+
+    public Engine(String capacity, String mark, String power, String type, boolean isRun) {
+        this.capacity = capacity;
+        this.mark = mark;
+        this.power = power;
+        this.type = type;
+        this.isRun = isRun;
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(capacity, mark, power, type, isRun);
+    }
 
 }
+
