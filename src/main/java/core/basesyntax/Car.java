6b0362bccf1b398a25551fe38c1210a9703f9c3c
Andrey Tanchak <tanchak12@gmail.com>
2020-04-02 15:53:22 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private int wight;
+    private int height;
+    private int length;
+    private int weight;
 
+    public Car(Engine engine, int wight, int height, int length, int weight) {
+        this.engine = engine;
+        this.wight = wight;
+        this.height = height;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    @Override
+    public Object clone() {
+        return new Car(engine,wight,height, length, weight);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int price;
+    private int capacity;
+    private int power;
+    private String fuelGrade;
+    private String supplySystem;
 
+    public Engine(int price, int capacity, int power, String fuelGrade, String supplySystem) {
+        this.price = price;
+        this.capacity = capacity;
+        this.power = power;
+        this.fuelGrade = fuelGrade;
+        this.supplySystem = supplySystem;
+    }
+
+    @Override
+    public Object clone() {
+        return new Engine(price, capacity, power, fuelGrade, supplySystem);
+    }
 }
