e09b6d484fa51b778ea9f5229b795bb47a56a16a
Vinglish <khan.ildar94@gmail.com>
2020-04-02 17:19:31 +0300

    clonable project

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int wheels;
+    private int doors;
+    private boolean conditioner;
+    private String color;
+    private Engine engine;
 
+    public Car(int wheels, int doors, boolean conditioner, String color, Engine engine) {
+        this.wheels = wheels;
+        this.doors = doors;
+        this.conditioner = conditioner;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public Car clone() {
+        return new Car(wheels, doors, conditioner, color, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String fuel;
+    private int horsepower;
+    private int volume;
+    private int cylinder;
+    private int newtonMeter;
 
+    public Engine(String fuel, int horsepower, int volume, int cylinder, int newtonMeter) {
+        this.fuel = fuel;
+        this.horsepower = horsepower;
+        this.volume = volume;
+        this.cylinder = cylinder;
+        this.newtonMeter = newtonMeter;
+    }
+
+    public Engine clone() {
+        return new Engine(fuel, horsepower, volume, cylinder, newtonMeter);
+    }
 }
