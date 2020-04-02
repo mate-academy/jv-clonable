14bd8365cbea30ffc440953a618ad36b4dd89422
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-02 17:48:30 +0300

    added solution for homework 5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private int wheelsCount;
+    private String manufacturer;
+    private String model;
+    private int age;
+    private int length;
+    private Engine engine;
+
+    public Car(int wheelsCount, String manufacturer,
+               String model, int age, int length,
+               Engine engine) {
+        this.wheelsCount = wheelsCount;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.age = age;
+        this.length = length;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(wheelsCount, manufacturer, model, age, length, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int age;
+    private String manufacturer;
+    private String model;
+    private int width;
+    private int length;
 
+    public Engine(int age, String manufacturer, String model, int width, int length) {
+        this.age = age;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.width = width;
+        this.length = length;
+    }
+
+    public Engine clone() {
+        return new Engine(age, manufacturer, model, width, length);
+    }
 }
