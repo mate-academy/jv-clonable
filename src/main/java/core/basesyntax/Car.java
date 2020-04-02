91caba0f09efc098bb359f2113738d44629dff9d
Nick97-git <nickarchangel97@gmail.com>
2020-04-02 11:57:43 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private Engine engine;
+    private String name;
+    private String model;
+    private String color;
+    private String numberOfCar;
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car cloneOfCar = (Car) super.clone();
+        cloneOfCar.engine = this.engine.clone();
+        return cloneOfCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private int levelOfNoise;
+    private int numOfCylinders;
+    private int power;
+    private String typeOfFuel;
+    private String typeOfCooling;
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
