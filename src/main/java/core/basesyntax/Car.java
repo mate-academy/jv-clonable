3fa3d4e94c3df28a44cfc3dbf0ce0c5d2064fea1
maks45 <maksim.durov45@gmail.com>
2020-04-02 14:05:10 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String colour;
+    private int doorsCount;
+    private int wheelsCount;
+    private String brand;
 
+    public Car(Engine engine, String colour, int doorsCount, int wheelsCount, String brand) {
+        this.engine = engine;
+        this.colour = colour;
+        this.doorsCount = doorsCount;
+        this.wheelsCount = wheelsCount;
+        this.brand = brand;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getDoorsCount() {
+        return doorsCount;
+    }
+
+    public void setDoorsCount(int doorsCount) {
+        this.doorsCount = doorsCount;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(
+                engine.clone(),
+                colour,
+                doorsCount,
+                wheelsCount,
+                brand
+        );
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private int weight;
+    private int maxRpm;
+    private String fuel;
+    private String color;
 
+    public Engine(int power, int weight, int maxRpm, String fuel, String color) {
+        this.power = power;
+        this.weight = weight;
+        this.maxRpm = maxRpm;
+        this.fuel = fuel;
+        this.color = color;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getMaxRpm() {
+        return maxRpm;
+    }
+
+    public void setMaxRpm(int maxRpm) {
+        this.maxRpm = maxRpm;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(
+             power,
+             weight,
+             maxRpm,
+             fuel,
+             color
+        );
+    }
 }
