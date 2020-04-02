4f0042846df58aca9286adb820e01b121419c192
Popovych Yurii <popovychyb@gmail.com>
2020-04-02 17:06:06 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private String type;
+    private int capacity;
+    private Engine engine;
 
+    public Car(String brand, String model, String type, int capacity, Engine engine) {
+        this.brand = brand;
+        this.model = model;
+        this.type = type;
+        this.capacity = capacity;
+        this.engine = engine;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand, model, type, capacity, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int powerOutput;
+    private String fuelType;
+    private int gears;
+    private int topSpeed;
+    private int size;
 
+    public Engine(int powerOutput, String fuelType, int gears, int topSpeed, int size) {
+        this.powerOutput = powerOutput;
+        this.fuelType = fuelType;
+        this.gears = gears;
+        this.topSpeed = topSpeed;
+        this.size = size;
+    }
+
+    public int getPowerOutput() {
+        return powerOutput;
+    }
+
+    public void setPowerOutput(int powerOutput) {
+        this.powerOutput = powerOutput;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(powerOutput, fuelType, gears, topSpeed, size);
+    }
 }
