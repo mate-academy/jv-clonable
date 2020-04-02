8c23b12efc0d56ca6ff68efebb3e3132ae04e5a7
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-02 13:36:54 +0300

    Changed solution.

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -7,8 +7,16 @@ public class Engine implements Cloneable {
     private int power;
     private int price;
 
+    public Engine(int ag, String typpe, int number, int pow, int pr) {
+        age = ag;
+        type = typpe;
+        serialNumber = number;
+        power = pow;
+        price = pr;
+    }
+
     @Override
     protected Engine clone() throws CloneNotSupportedException {
-        return new Engine();
+        return new Engine(age, type, serialNumber, power, price);
     }
 }
