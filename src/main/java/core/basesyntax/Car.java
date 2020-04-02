57ed6dab19ed2e88f535f3be4075ee9f7ea50d77
NikolayAbysov <nikolay.abysov@ukr.net>
2020-04-02 13:54:40 +0300

    Cloneable interface implemented

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String brand;
+    private String model;
+    private String color;
+    private long vin;
+    private Engine engine;
+
+    public Car(String brand, String model, String color, long vin, Engine engine) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.vin = vin;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand, model, color, vin, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private long serial;
+    private String fuel;
+    private int volume;
+    private int torque;
 
+    public Engine(String model, long serial, String fuel, int volume, int torque) {
+        this.model = model;
+        this.serial = serial;
+        this.fuel = fuel;
+        this.volume = volume;
+        this.torque = torque;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Engine(model, serial, fuel, volume, torque);
+    }
 }
