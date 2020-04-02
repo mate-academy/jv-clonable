0316df8697aba6f1c0d172211441d173001455da
Serhii Maksymov <serhio.maksymov@gmail.com>
2020-04-02 16:08:19 +0300

    added clone's solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,36 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String modelCare;
+    private int carSpeed;
+    private String carColor;
+    private int weight;
+    private Engine engine;
 
+    public Car() {
+    }
+
+    public Car(String modelCare, int carSpeed, String carColor, int weight, Engine carEngine) {
+        this.modelCare = modelCare;
+        this.carSpeed = carSpeed;
+        this.carColor = carColor;
+        this.weight = weight;
+        this.engine = carEngine;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car) super.clone();
+        Engine newEngine = this.getEngine().clone();
+        newCar.setEngine(newEngine);
+        return newCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String engineType;
+    private int cylindersNumber;
+    private String transmissionType;
+    private int enginePower;
+    private int engineTorque;
 
+    public Engine() {
+    }
+
+    public Engine(String engineType, int cylindersNumber, String transmissionType,
+                  int enginePower, int engineTorque) {
+        this.engineType = engineType;
+        this.cylindersNumber = cylindersNumber;
+        this.transmissionType = transmissionType;
+        this.enginePower = enginePower;
+        this.engineTorque = engineTorque;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
