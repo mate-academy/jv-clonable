7b530fe6be8051bf163d13c0ac460c494b02dc55
6ALLIKA <skorik009@gmail.com>
2020-04-02 15:14:42 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int howOld;
+    private String name;
+    private String type;
+    private Engine engine;
+    private int lastTechnicalCheckYear;
 
+    public Car(int howOld, String name, String type, Engine engine, int lastTechnicalCheckYear) {
+        this.howOld = howOld;
+        this.name = name;
+        this.type = type;
+        this.engine = engine;
+        this.lastTechnicalCheckYear = lastTechnicalCheckYear;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(howOld, name, type, engine, lastTechnicalCheckYear);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int horsePower;
+    private String name;
+    private String type;
+    private int speed;
+    private boolean isWorking;
 
+    public Engine(int horsePower, String name, String type, int speed, boolean isWorking) {
+        this.horsePower = horsePower;
+        this.name = name;
+        this.type = type;
+        this.speed = speed;
+        this.isWorking = isWorking;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(horsePower, name, type, speed, isWorking);
+    }
 }
