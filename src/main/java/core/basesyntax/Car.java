33b0a1d131ee574ea3c961c18ce12cd9224392d0
olehedza <olehedza@gmail.com>
2020-04-03 13:42:44 +0300

    implement clone method for two classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String formFactor;
+    private String color;
+    private double maxSpeed;
+    private byte doorsCount;
+    private Engine engine;
 
+    public Car(String formFact, String col, double maxSp,
+               byte doors, Engine eng) {
+        formFactor = formFact;
+        color = col;
+        maxSpeed = maxSp;
+        doorsCount = doors;
+        engine = eng;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(formFactor, color, maxSpeed, doorsCount, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String power;
+    private String fuelType;
+    private double hourlyFuelConsumption;
+    private byte cylindersCount;
+    private String engineDisplacement;
 
+    public Engine(String pow, String fuelT,
+                  double hfc,
+                  byte cylCount,
+                  String engineDis) {
+        power = pow;
+        fuelType = fuelT;
+        hourlyFuelConsumption = hfc;
+        cylindersCount = cylCount;
+        engineDisplacement = engineDis;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(power, fuelType, hourlyFuelConsumption,
+                cylindersCount, engineDisplacement);
+    }
 }
