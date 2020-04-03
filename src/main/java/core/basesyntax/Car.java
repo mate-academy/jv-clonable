9f9d8162a25fff49c70b2147c7c51fcad625da97
Svitlana Mykolaienko <svetlana.mikolaenko@gmail.com>
2020-04-03 17:22:25 +0300

    add solution clonable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String type;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private String transmission;
+    private Engine engine;
 
+    public Car(String type, String brand, String model, int year,
+               String color, String transmission, Engine engine) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.transmission = transmission;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(type, brand, model, year, color, transmission, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String fuel;
+    private float capacity;
+    private int power;
+    private float fuelConsumption;
+    private int cylinderQuantity;
 
+    public Engine(String fuel, float capacity, int power,
+                  float fuelConsumption, int cylinderQuantity) {
+        this.fuel = fuel;
+        this.capacity = capacity;
+        this.power = power;
+        this.fuelConsumption = fuelConsumption;
+        this.cylinderQuantity = cylinderQuantity;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(fuel, capacity, power, fuelConsumption, cylinderQuantity);
+    }
 }
