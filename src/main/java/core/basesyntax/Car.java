79ba59250c23930744607471846d0861be63bed0
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-03 08:28:34 +0300

    hw_5_1_done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private Engine engine;
+    private String driveUnit;
+    private int numberOfSeats;
+
+    public Car(String model, String color, Engine engine, String driveUnit, int numberOfSeats) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+        this.driveUnit = driveUnit;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(model, color, engine, driveUnit, numberOfSeats);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,25 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+
+    private String enginesType;
+    private String fuelGrade;
+    private int engineCapacity;
+    private int valvesPerCylinder;
+    private int power;
+
+    public Engine(String enginesType, String fuelGrade,
+                   int engineCapacity, int valvesPerCylinder, int power) {
+        this.enginesType = enginesType;
+        this.fuelGrade = fuelGrade;
+        this.engineCapacity = engineCapacity;
+        this.valvesPerCylinder = valvesPerCylinder;
+        this.power = power;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(enginesType, fuelGrade, engineCapacity, valvesPerCylinder, power);
+    }
 
 }
