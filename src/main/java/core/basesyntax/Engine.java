83207356433b4d4be74ab7aced5e91cb4028ccc4
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-03 11:06:30 +0300

    changed Engine parameters

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,22 +1,22 @@
 package core.basesyntax;
 
 public class Engine implements Cloneable {
-    private int param1;
-    private int param2;
-    private int param3;
-    private int param4;
-    private int param5;
+    private int height;
+    private int weight;
+    private int length;
+    private int power;
+    private int cost;
 
-    public Engine(int param1, int param2, int param3, int param4, int param5) {
-        this.param5 = param5;
-        this.param4 = param4;
-        this.param3 = param3;
-        this.param2 = param2;
-        this.param1 = param1;
+    public Engine(int height, int weight, int length, int power, int cost) {
+        this.cost = cost;
+        this.power = power;
+        this.length = length;
+        this.weight = weight;
+        this.height = height;
     }
 
     @Override
     public Engine clone() {
-        return new Engine(param1, param2, param3, param4, param5);
+        return new Engine(height, weight, length, power, cost);
     }
 }
