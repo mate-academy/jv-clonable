8011229855c0da4eb119dfedde22f4ff10e7f293
VovkAA <vovkandrew@gmail.com>
2020-04-03 14:39:37 +0300

    Done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,30 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String manufacturer;
+    private String model;
+    private int maxSpeed;
+    private int length;
+    private String color;
+    private Engine engine;
 
+    public Car(String manufacturer,
+               String model,
+               int maxSpeed,
+               int length,
+               String color,
+               Engine engine) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.length = length;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(manufacturer, model, maxSpeed, length, color, engine);
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double volume;
+    private String fuel;
+    private Boolean hybrid;
+    private int size;
+    private int maxDistance;
 
+    public Engine(double volume, String fuel, Boolean hybrid, int size, int maxDistance) {
+        this.volume = volume;
+        this.fuel = fuel;
+        this.hybrid = hybrid;
+        this.size = size;
+        this.maxDistance = maxDistance;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(volume, fuel, hybrid, size, maxDistance);
+    }
 }
