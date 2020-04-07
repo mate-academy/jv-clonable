2f7648330c5ce61d69c58d34bc1cc74b6613ab59
Oleksandr58 <kovaliukoleksandr@gmail.com>
2020-04-07 09:29:22 +0300

    Implements Cloneable for Car and Engine classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private int seats;
+    private int doors;
+    private String model;
+    private Engine engine;
 
+    public Car(String color, int seats, int doors,
+               String model, Engine engine) {
+        this.color = color;
+        this.seats = seats;
+        this.doors = doors;
+        this.model = model;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(color, seats, doors, model, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String fuel;
+    private float capacity;
+    private int power;
+    private int year;
+    private String code;
 
+    public Engine(String fuel, float capacity, int power,
+               int year, String code) {
+        this.fuel = fuel;
+        this.capacity = capacity;
+        this.power = power;
+        this.year = year;
+        this.code = code;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(fuel, capacity, power, year, code);
+    }
 }
