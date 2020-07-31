4a3a071756597f76be7b76d57cf4358414490635
Roman <matviichuk.office@gmail.com>
2020-07-31 15:40:22 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String nameOwner;
+    private String color;
+    private String brand;
+    private int numbers;
+    private int parkingLot;
+    private Engine engine;
 
+    public Car(String nameOwner, String color, String brand, int numbers,
+               int parkingLot, Engine clone) {
+        this.nameOwner = nameOwner;
+        this.color = color;
+        this.brand = brand;
+        this.numbers = numbers;
+        this.parkingLot = parkingLot;
+    }
+
+    protected Car clone() {
+        return new Car(nameOwner, color, brand, numbers, parkingLot, engine.clone());
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private String powerSystem;
+    private String environmentStandarts;
+    private int capacity;
+    private int yearOfIssue;
 
+    public Engine(String type, String powerSystem, String environmentStandarts,
+                  int capacity, int yearOfIssue) {
+        this.type = type;
+        this.powerSystem = powerSystem;
+        this.environmentStandarts = environmentStandarts;
+        this.capacity = capacity;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    protected Engine clone() {
+        return new Engine(type, powerSystem, environmentStandarts, capacity, yearOfIssue);
+    }
 }
