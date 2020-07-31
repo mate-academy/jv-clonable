8d6e27db29846661d801d2b9706e22ceb0903157
Kostian <pyteec.ua@i.ua>
2020-07-31 14:25:55 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String carBrand;
+    private int countOfDoors;
+    private String type;
+    private Engine engine;
 
+    public Car(String color, String carBrand, int countOfDoors, String type, Engine engine) {
+        this.color = color;
+        this.carBrand = carBrand;
+        this.countOfDoors = countOfDoors;
+        this.type = type;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(color, carBrand, countOfDoors, type, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int maxSpeed;
+    private double engineCapacity;
+    private int fuelConsumption;
+    private int torque;
+    private String type;
 
+    public Engine(int maxSpeed, double engineCapacity,
+                  int fuelConsumption, int torque, String type) {
+        this.maxSpeed = maxSpeed;
+        this.engineCapacity = engineCapacity;
+        this.fuelConsumption = fuelConsumption;
+        this.torque = torque;
+        this.type = type;
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(maxSpeed, engineCapacity, fuelConsumption, torque, type);
+    }
 }
