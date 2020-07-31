d60c79feb0e331715cc4e1fc1f3da25954f4a359
Laszlo Petechuk <frostpv@gmail.com>
2020-07-31 16:32:16 +0300

    my solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String colour;
+    private String brand;
+    private int speed;
+    private int price;
+    private Engine engine;
 
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return (Car) super.clone();
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private String transmission;
+    private String injection;
+    private int gears;
+    private double enginVolume;
 
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getInjection() {
+        return injection;
+    }
+
+    public void setInjection(String injection) {
+        this.injection = injection;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public double getEnginVolume() {
+        return enginVolume;
+    }
+
+    public void setEnginVolume(double enginVolume) {
+        this.enginVolume = enginVolume;
+    }
 }
