abdffa376ca75477315b0a239319d1aa11128a38
glones <polivanchuk2013@gmail.com>
2020-07-31 14:31:38 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,36 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private String model;
+    private String manufactureYear;
+    private int maxSpeed;
+    private Engine engine;
 
+    public Car(String name, String color, String model,
+               String manufactureYear, int maxSpeed, Engine engine) {
+        this.name = name;
+        this.color = color;
+        this.model = model;
+        this.manufactureYear = manufactureYear;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car) super.clone();
+        Engine newEngine = this.getEngine().clone();
+        newCar.setEngine(newEngine);
+        return newCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private int volumeOfChamber;
+    private String combustion;
+    private int compression;
+    private int power;
 
+    public Engine(String name, int volumeOfChamber, String combustion, int compression, int power) {
+        this.name = name;
+        this.volumeOfChamber = volumeOfChamber;
+        this.combustion = combustion;
+        this.compression = compression;
+        this.power = power;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(name, volumeOfChamber, combustion, compression, power);
+    }
 }
