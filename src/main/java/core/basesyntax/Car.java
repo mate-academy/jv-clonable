16b00e324874d19f49002c4abc7dc2e7ef9f50a1
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-07-31 14:09:25 +0300

    hw5-1_done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private int yearOfMade;
+    private int mileage;
+    private int weight;
+    private Engine engine;
 
+    public Car(String brand, String model, int yearOfMade, int mileage, int weight, Engine engine) {
+        this.brand = brand;
+        this.model = model;
+        this.yearOfMade = yearOfMade;
+        this.mileage = mileage;
+        this.weight = weight;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        return new Car(brand, model, yearOfMade, mileage, weight, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String specification;
+    private int yearOfMade;
+    private String countryOfMade;
+    private int serialNumber;
 
+    public Engine(String model, String specification, int yearOfMade,
+                  String countryOfMade, int serialNumber) {
+        this.model = model;
+        this.specification = specification;
+        this.yearOfMade = yearOfMade;
+        this.countryOfMade = countryOfMade;
+        this.serialNumber = serialNumber;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return new Engine(model, specification, yearOfMade, countryOfMade, serialNumber);
+    }
 }
