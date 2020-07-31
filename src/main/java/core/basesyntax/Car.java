0a86bb8c9827ef203e876f9cef11dca768d17dcd
FYarkiy <flx_jb@yahoo.com>
2020-07-31 15:35:30 +0300

    Create two classes and redifine Clone method

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private String version;
+    private int numberOfDoors;
+    private Engine engine;
 
+    public Car(String brand, String version, int numberOfDoors, Engine engine) {
+        this.brand = brand;
+        this.version = version;
+        this.numberOfDoors = numberOfDoors;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        clone().brand = brand;
+        clone().model = model;
+        clone().engine = engine;
+        clone().numberOfDoors = numberOfDoors;
+        clone().version = version;
+        return clone();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private String modelName;
+    private boolean electric;
+    private int yearModel;
+    private String type;
 
+    private Engine(int power, String modelName, boolean electric, int yearModel, String type) {
+        this.power = power;
+        this.modelName = modelName;
+        this.electric = electric;
+        this.yearModel = yearModel;
+        this.type = type;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        clone().power = power;
+        clone().modelName = modelName;
+        clone().electric = electric;
+        clone().yearModel = yearModel;
+        clone().type = type;
+        return clone();
+    }
 }
