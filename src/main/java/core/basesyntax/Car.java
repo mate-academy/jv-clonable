c5f8ed04e6c4607a2295bac2d0c9edd65d67001b
Your Name <i.dymytreyeva@gmail.com>
2020-07-31 14:12:18 +0300

    solution added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,31 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private int maxSpeed;
+    private String model;
+    private String color;
+    private int doorsAmount;
 
+    public Car(int maxSpeed, String model, String color, int doorsAmount, Engine engine) {
+        this.maxSpeed = maxSpeed;
+        this.color = color;
+        this.doorsAmount = doorsAmount;
+        this.engine = engine;
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(maxSpeed, model, color, doorsAmount, engine.clone());
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int horsesPower;
+    private String model;
+    private String code;
+    private int numbersOfCylinders;
+    private int volume;
 
+    public Engine(int horsesPower, String model, String code, int numbersOfCylinders, int volume) {
+        this.horsesPower = horsesPower;
+        this.model = model;
+        this.code = code;
+        this.numbersOfCylinders = numbersOfCylinders;
+        this.volume = volume;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(horsesPower, model, code, numbersOfCylinders, volume);
+    }
 }
