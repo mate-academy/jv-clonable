176c9ca35a6b0da0097b995c9d85b5fee9e0fb3a
Artem Vremere <vremere_a@ukr.net>
2020-07-31 13:35:00 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private Engine engine;
+    private int year;
+    private double speed;
 
+    public Car(String model, String color, Engine engine, int year, double speed) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+        this.year = year;
+        this.speed = speed;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    protected Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car)super.clone();
+        newCar.setEngine((Engine) newCar.getEngine().clone());
+        return newCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,53 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private int year;
+    private double horsePower;
+    private String type;
+    private double volume;
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(double horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
