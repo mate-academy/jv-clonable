dcbd2a0edeed909527ed05876fcafa8768797d32
MaliukDaria <maliuk.dasha@gmail.com>
2020-07-31 14:16:55 +0300

    implemented method Clone()

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private int numberOfDoors;
+    private Colour colour;
+    private Engine engine;
 
+    public Car(String brand, String model, int numberOfDoors, Colour colour, Engine engine) {
+        this.brand = brand;
+        this.model = model;
+        this.numberOfDoors = numberOfDoors;
+        this.colour = colour;
+        this.engine = engine;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
+
+    public void setColour(Colour colour) {
+        this.colour = colour;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand,model,numberOfDoors,colour,engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int numberOfCylinders;
+    private int power;
+    private String fuel;
+    private boolean isTurbocharger;
 
+    public Engine(String type, int numberOfCylinders,
+                  int power, String fuel, boolean isTurbocharger) {
+        this.type = type;
+        this.numberOfCylinders = numberOfCylinders;
+        this.power = power;
+        this.fuel = fuel;
+        this.isTurbocharger = isTurbocharger;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public boolean isTurbocharger() {
+        return isTurbocharger;
+    }
+
+    public void setTurbocharger(boolean turbocharger) {
+        isTurbocharger = turbocharger;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
