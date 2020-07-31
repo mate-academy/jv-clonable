8611fc29a3f5bde57e2a5bc3e85c1a92f36f03f9
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-07-31 15:58:06 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private int mileage;
+    private int yearMade;
+    private boolean isInsured;
+    private Engine engine;
 
+    public Car(String brand,
+               int mileage,
+               int yearMade,
+               boolean isInsured,
+               Engine engine) {
+        this.brand = brand;
+        this.mileage = mileage;
+        this.yearMade = yearMade;
+        this.isInsured = isInsured;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(brand, mileage, yearMade, isInsured, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,27 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String brand;
+    private String madeIn;
+    private int sizeInLiters;
+    private int yearMade;
+    private boolean needsRepair;
+
+    public Engine(String brand,
+                  String madeIn,
+                  int sizeInLiters,
+                  int yearMade,
+                  boolean needsRepair) {
+        this.brand = brand;
+        this.madeIn = madeIn;
+        this.sizeInLiters = sizeInLiters;
+        this.yearMade = yearMade;
+        this.needsRepair = needsRepair;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(brand, madeIn, sizeInLiters, yearMade, needsRepair);
+    }
 
 }
