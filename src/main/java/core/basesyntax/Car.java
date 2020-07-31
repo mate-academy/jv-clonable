df0214b22e9d57b10f998dd601c658b75904b591
Daria <dariyapikul@gmail.com>
2020-07-31 15:19:48 +0300

    solution added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,29 @@
 package core.basesyntax;
 
-public class Car {
+public class Car extends Engine implements Cloneable {
 
+    private String model;
+    private String type;
+    private String countryOfProduction;
+    private int yearOfProduction;
+    private int price;
+    private Engine engine;
+
+    public Car build() {
+        Car car = new Car();
+        car.model = Car.this.model;
+        car.type = Car.this.type;
+        car.countryOfProduction = Car.this.countryOfProduction;
+        car.yearOfProduction = Car.this.yearOfProduction;
+        car.price = Car.this.price;
+        car.engine = Car.this.engine.build();
+        return car;
+    }
+
+    @Override
+    public Object clone() {
+        Car car = new Car();
+        car.build();
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,29 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private String model;
+    private String driveUnit;
+    private String transmission;
+    private int volume;
+    private int maxPower;
+
+    public Engine build() {
+
+        Engine engine = new Engine();
+        engine.model = Engine.this.model;
+        engine.driveUnit = Engine.this.driveUnit;
+        engine.transmission = Engine.this.transmission;
+        engine.volume = Engine.this.volume;
+        engine.maxPower = Engine.this.maxPower;
+
+        return engine;
+    }
+
+    @Override
+    public Object clone() {
+        Engine engine = new Engine();
+        engine.build();
+        return engine;
+    }
 }
