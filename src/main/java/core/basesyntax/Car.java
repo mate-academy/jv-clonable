98c9e28fc995edb42d5c731a511040032d4d0aea
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-07-31 14:31:15 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String mark;
+    private String color;
+    private int speed;
+    private int numberOfPassengers;
+    private Engine engine;
 
+    public Car(String mark, String color, int speed, int numberOfPassengers, Engine engine) {
+        this.mark = mark;
+        this.color = color;
+        this.speed = speed;
+        this.numberOfPassengers = numberOfPassengers;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() {
+        return new Car(mark, color, speed, numberOfPassengers, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,32 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private double engineCapacity;
+    private double noiseLevel;
+    private int serialNumber;
+    private double weight;
+    private int torque;
+
+    public Engine(double engineCapacity, double noiseLevel,
+                  int serialNumber, double weight, int torque) {
+        this.engineCapacity = engineCapacity;
+        this.noiseLevel = noiseLevel;
+        this.serialNumber = serialNumber;
+        this.weight = weight;
+        this.torque = torque;
+    }
+
+    public Engine(Engine engine) {
+        this.engineCapacity = engine.engineCapacity;
+        this.noiseLevel = engine.noiseLevel;
+        this.serialNumber = engine.serialNumber;
+        this.weight = engine.weight;
+        this.torque = engine.torque;
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(engineCapacity, noiseLevel, serialNumber, weight, torque);
+    }
 }
