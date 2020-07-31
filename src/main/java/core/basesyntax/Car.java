e0c43d7a82d910a4f9deab04e3919d8f9f72a53c
Vas35 <vas0977515005@gmail.com>
2020-07-31 18:33:16 +0300

    solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,28 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String glass;
+    private String headlights;
+    private String tank;
+    private String wheel;
+    private Engine engine;
+
+    public Car(String glass, String headlights, String tank, String wheel, Engine engine) {
+        this.glass = glass;
+        this.headlights = headlights;
+        this.tank = tank;
+        this.wheel = wheel;
+        this.engine = engine;
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+        Car car = new Car(glass, headlights, tank, wheel, engine.clone());
+        return car;
+
+    }
 
 }
+
+
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,25 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int temperature;
+    private int volume;
+    private int pressure;
+    private int speed;
+    private int age;
 
+    public Engine(int temperature, int volume, int pressure, int speed, int age) {
+        this.temperature = temperature;
+        this.volume = volume;
+        this.pressure = pressure;
+        this.speed = speed;
+        this.age = age;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        Engine engine = new Engine(temperature, volume, pressure, speed, age);
+        return engine;
+    }
 }
+
+
