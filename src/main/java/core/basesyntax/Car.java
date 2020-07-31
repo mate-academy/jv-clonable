0ce62c78e933ff4ea3992b9c140074dc229e0c09
Alkey <Sirj777sirj@gmail.com>
2020-07-31 12:55:41 +0300

    homework 5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,21 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int maxSpeed;
+    private String transmission;
+    private String color;
+    private String mark;
+    private Engine carEngine;
 
+    public Car(int maxSpeed, String transmission, String color, String mark, Engine carEngine) {
+        this.maxSpeed = maxSpeed;
+        this.transmission = transmission;
+        this.color = color;
+        this.mark = mark;
+        this.carEngine = carEngine;
+    }
+
+    public Car clone(Car car) {
+        return new Car(car.maxSpeed, car.transmission, car.color, car.mark, car.carEngine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int horsePowers;
+    private String serialNumber;
+    private String typeOfFuel;
+    private double volume;
+    private int weight;
 
+    public Engine(int horsePowers, String serialNumber,
+                  String typeOfFuel, double volume, int weight) {
+        this.horsePowers = horsePowers;
+        this.serialNumber = serialNumber;
+        this.typeOfFuel = typeOfFuel;
+        this.volume = volume;
+        this.weight = weight;
+    }
+
+    public Engine clone(Engine engine) {
+        return new Engine(engine.horsePowers, engine.serialNumber,
+                engine.typeOfFuel, engine.volume, weight);
+    }
 }
