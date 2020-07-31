6fc21153c886df75d2a19af459dd739c485848d6
Martynice <marticiy@ukr.net>
2020-07-31 14:39:16 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,67 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String model;
+    private int number;
+    private String color;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String model, int number, String color, int maxSpeed, Engine engine) {
+        this.model = model;
+        this.number = number;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public void setModel() {
+        this.model = model;
+    }
+
+    public void setNumber() {
+        this.number = number;
+    }
+
+    public void setColor() {
+        this.color = color;
+    }
+
+    public void setMaxSpeed() {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setEngine() {
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public String move() {
+        return "Car is moving";
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(model, number, color, maxSpeed, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,68 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+
+    private int id;
+    private int horsepower;
+    private double weight;
+    private double volume;
+    private String guarantee;
+
+    public Engine(int id, int horsepower, double weight, double volume, String guarantee) {
+        this.id = id;
+        this.horsepower = horsepower;
+        this.weight = weight;
+        this.volume = volume;
+        this.guarantee = guarantee;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public void setHorsepower() {
+        this.horsepower = horsepower;
+    }
+
+    public void setWeight() {
+        this.weight = weight;
+    }
+
+    public void setVolume() {
+        this.volume = volume;
+    }
+
+    public void setGuarantee() {
+        this.guarantee = guarantee;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public String getGuarantee() {
+        return guarantee;
+    }
+
+    public String startEngine() {
+        return "Engine is started";
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(id, horsepower, weight, volume, guarantee);
+    }
 
 }
