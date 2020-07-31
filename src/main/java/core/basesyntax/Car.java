81784179a99c90ce5fa01e96510204dbbd65e9f1
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-31 13:24:16 +0300

    solution added

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,36 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+
+    private String color;
+    private String model;
+    private int year;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String color, String model, int year,
+               int maxSpeed, Engine engine) {
+        this.color = color;
+        this.engine = engine;
+        this.maxSpeed = maxSpeed;
+        this.year = year;
+        this.model = model;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Car clone() throws CloneNotSupportedException {
+
+        return new Car(color, model,year,maxSpeed,engine);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,34 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int cylinder;
+    private String model;
+    private String speedTransmission;
+    private int horsePower;
+    private int fuelTankCapacity;
+
+    public Engine(int cylinder, String model, String speedTransmission,
+                  int horsePower, int fuelTankCapacity) {
+        this.cylinder = cylinder;
+        this.model = model;
+        this.speedTransmission = speedTransmission;
+        this.horsePower = horsePower;
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return new Engine(cylinder, model, speedTransmission, horsePower, fuelTankCapacity);
+    }
 
 }
