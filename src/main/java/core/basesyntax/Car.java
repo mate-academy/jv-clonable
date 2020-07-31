23babd66b5fa1a1a452a0448d5d9e8028a56b3b5
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-07-31 15:19:37 +0300

    Add solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,28 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int id;
+    private int weight;
+    private String model;
+    private String colour;
+    private Engine engine;
 
+    public Car(int id, int weight, String model, String colour, Engine engine) {
+        this.id = id;
+        this.weight = weight;
+        this.model = model;
+        this.colour = colour;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        // var 1
+        //return new Car(id, weight, model, colour, engine);
+
+        // var 2
+        Car car = (Car) super.clone();
+        car.engine = (Engine) car.engine.clone();
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,30 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int id;
+    private int weight;
+    private String model;
+    private String color;
+    private int power;
 
+    public Engine(int id, int weight, String model, String color, int power) {
+        this.id = id;
+        this.weight = weight;
+        this.model = model;
+        this.color = color;
+        this.power = power;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        // var 1
+        //return new Engine(id, weight, model, color, power);
+
+        // var 2
+        // Engine engine = (Engine) super.clone();
+        // return engine;
+
+        // var 3
+        return super.clone();
+    }
 }
