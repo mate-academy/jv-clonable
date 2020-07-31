4f7e7c19f60d8e9657ae3e345d4ee2595b8e5d83
Borisko130 <krimenyukb@gmail.com>
2020-07-31 15:31:10 +0300

    Homework 5.1 completed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Engine engine = new Engine(100, 4, 20, 200, 40);
-        Car car1 = new Car("Volvo", 100, 4, false, engine);
+        Car volvo = new Car("Volvo", 100, 4, false, engine);
 
-        Car clonedCar = car1.clone();
-        clonedCar.setModel("Citroen");
+        Car citroen = volvo.clone();
+        citroen.setModel("Citroen");
 
-        car1.describe();
-        clonedCar.describe();
+        volvo.describe();
+        citroen.describe();
     }
 }
