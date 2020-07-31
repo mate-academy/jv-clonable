8fd936aabc5f9808e8e0a9afa4ffa726321c9487
Andriy <andriy.lagun@gmail.com>
2020-07-31 11:44:25 +0300

    Implemented clone methods in Car, Engine classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String producer;
+    private int countOfDoors;
+    private Engine engine;
+    private String color;
+    private String type;
 
+    public Car(String producer, int countOfDoors, Engine engine, String color, String type) {
+        this.producer = producer;
+        this.countOfDoors = countOfDoors;
+        this.engine = engine;
+        this.color = color;
+        this.type = type;
+    }
+
+    @Override
+    protected Object clone() {
+        return new Car(producer,countOfDoors,engine,color,type);
+    }
 }
