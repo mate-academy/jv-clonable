5d073202458275619f52b8bd90469c9db2505a71
Leonid <rommel@ukr.net>
2020-07-31 20:42:35 +0300

    task done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String model;
+    private Engine engine;
+    private int weight;
+    private int manufacturedYear;
 
+    public Car(String brand, String model, Engine engine, int weight, int manufacturedYear) {
+        this.brand = brand;
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.manufacturedYear = manufacturedYear;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Car(brand, model, engine, weight, manufacturedYear);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getManufacturedYear() {
+        return manufacturedYear;
+    }
+
+    public void setManufacturedYear(int manufacturedYear) {
+        this.manufacturedYear = manufacturedYear;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String brand;
+    private String model;
+    private int volume;
+    private int power;
+    private int numberOfCylinders;
 
+    public Engine(String brand, String model, int volume, int power, int numberOfCylinders) {
+        this.brand = brand;
+        this.model = model;
+        this.volume = volume;
+        this.power = power;
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Engine(brand, model, volume, power, numberOfCylinders);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
 }
