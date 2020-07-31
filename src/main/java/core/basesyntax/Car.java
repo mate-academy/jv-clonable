045fe0d4ad2b2949350f9aa5f4a94c6c12e4838a
Dm_Grt <grytsishin@gmail.com>
2020-07-31 14:43:31 +0300

    Added solution 5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,77 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private Engine engine;
+    private String color;
+    private String type;
+    private boolean isElectricCar;
 
+    public Car(String name, Engine engine, String color, String type, boolean isElectricCar) {
+        this.name = name;
+        this.engine = engine;
+        this.color = color;
+        this.type = type;
+        this.isElectricCar = isElectricCar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        if (engine != null) {
+            this.engine = engine;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color != null) {
+            this.color = color;
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        if (type != null) {
+            this.type = type;
+        }
+    }
+
+    public boolean isElectricCar() {
+        return isElectricCar;
+    }
+
+    public void setElectricCar(boolean electricCar) {
+        isElectricCar = electricCar;
+    }
+
+    @Override
+    public Car clone() {
+        Car car;
+        try {
+            car = (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            car = new Car(this.getName(), this.getEngine().clone(),
+                    this.getColor(), this.getType(), this.isElectricCar());
+        }
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,92 @@
 package core.basesyntax;
 
-public class Engine {
+import java.time.LocalDate;
 
+public class Engine implements Cloneable {
+    private int rpm;
+    private LocalDate engineHours;
+    private int coolantTemperature;
+    private int actualTorque;
+    private int turboPressure;
+    private String idleAnalysis;
+
+    public Engine(int rpm, LocalDate engineHours, int coolantTemperature,
+                  int actualTorque, int turboPressure, String idleAnalysis) {
+        this.rpm = rpm;
+        this.engineHours = engineHours;
+        this.coolantTemperature = coolantTemperature;
+        this.actualTorque = actualTorque;
+        this.turboPressure = turboPressure;
+        this.idleAnalysis = idleAnalysis;
+    }
+
+    public int getRpm() {
+        return rpm;
+    }
+
+    public void setRpm(int rpm) {
+        if (rpm != 0) {
+            this.rpm = rpm;
+        }
+    }
+
+    public LocalDate getEngineHours() {
+        return engineHours;
+    }
+
+    public void setEngineHours(LocalDate engineHours) {
+        if (engineHours != null) {
+            this.engineHours = engineHours;
+        }
+    }
+
+    public int getCoolantTemperature() {
+        return coolantTemperature;
+    }
+
+    public void setCoolantTemperature(int coolantTemperature) {
+        if (coolantTemperature != 0) {
+            this.coolantTemperature = coolantTemperature;
+        }
+    }
+
+    public int getActualTorque() {
+        return actualTorque;
+    }
+
+    public void setActualTorque(int actualTorque) {
+        if (actualTorque != 0) {
+            this.actualTorque = actualTorque;
+        }
+    }
+
+    public int getTurboPressure() {
+        return turboPressure;
+    }
+
+    public void setTurboPressure(int turboPressure) {
+        if (turboPressure != 0) {
+            this.turboPressure = turboPressure;
+        }
+    }
+
+    public String getIdleAnalysis() {
+        return idleAnalysis;
+    }
+
+    public void setIdleAnalysis(String idleAnalysis) {
+        if (idleAnalysis != null) {
+            this.idleAnalysis = idleAnalysis;
+        }
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Engine(this.getRpm(), this.getEngineHours(), this.getCoolantTemperature(),
+                    this.getActualTorque(), this.getTurboPressure(), this.getIdleAnalysis());
+        }
+    }
 }
