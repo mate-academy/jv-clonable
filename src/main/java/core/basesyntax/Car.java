7a5a99eb60e649529edcaf1efd12274a3f2a788e
romanHreits <romagreic@gmail.com>
2020-07-31 12:32:59 +0300

    ready solution hw-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private String carBrand;
+    private String yearOfProduction;
+    private String color;
+    private int maxSpeed;
+    private Engine engine;
+
+    public Car(String carBrand, String yearOfProduction,
+               String color, int maxSpeed, Engine engine) {
+        this.carBrand = carBrand;
+        this.yearOfProduction = yearOfProduction;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(carBrand, yearOfProduction, color, maxSpeed, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String typeOfEngine;
+    private String name;
+    private int power;
+    private String code;
+    private String producing;
 
+    public Engine(String typeOfEngine, String name,
+                  int power, String code, String producing) {
+        this.typeOfEngine = typeOfEngine;
+        this.name = name;
+        this.power = power;
+        this.code = code;
+        this.producing = producing;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(typeOfEngine, name, power, code, producing);
+    }
 }
