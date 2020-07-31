a0416e6aed373af7c5197d9e6a84a94fc1f93f1a
Viktor Nikolaev <nikolaev.vikt@gmail.com>
2020-07-31 16:13:35 +0300

    [ADD] solution hw-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String name;
+    private int maxSpeed;
+    private int maxWeight;
+    private Engine engine;
 
+    public Car(String model, String name, int maxSpeed, int maxWeight, Engine engine) {
+        this.model = model;
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+        this.maxWeight = maxWeight;
+        this.engine = engine;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new Car(model, name, maxSpeed, maxWeight, engine).clone();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String winCode;
+    private String model;
+    private int weight;
+    private int engineSpeed;
+    private int engineVolume;
 
+    public Engine(String winCode, String model, int weight, int engineSpeed, int engineVolume) {
+        this.winCode = winCode;
+        this.model = model;
+        this.weight = weight;
+        this.engineSpeed = engineSpeed;
+        this.engineVolume = engineVolume;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return new Engine(winCode, model, weight, engineSpeed, engineVolume).clone();
+    }
 }
