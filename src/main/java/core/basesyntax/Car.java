16c857f56615f79cdb30ec6799a40ec509502c33
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-07-31 15:52:02 +0300

    task 5.1 done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,73 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable{
+    private static long vinCodeCounter = 0;
+    private final long vinCode;
+    private String mark;
+    private String model;
+    private String typeOfCarcase;
+    private Engine engine;
 
+    public Car(String mark, String model, String typeOfCarcase, Engine engine) {
+        vinCode = vinCodeCounter++;
+        this.mark = mark;
+        this.model = model;
+        this.typeOfCarcase = typeOfCarcase;
+        this.engine = engine;
+    }
+
+    public long getVinCode() {
+        return vinCode;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getTypeOfCarcase() {
+        return typeOfCarcase;
+    }
+
+    public void setTypeOfCarcase(String typeOfCarcase) {
+        this.typeOfCarcase = typeOfCarcase;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "vinCode='" + vinCode + '\''
+                + ", mark='" + mark + '\''
+                + ", model='" + model + '\''
+                + ", typeOfCarcase='" + typeOfCarcase + '\''
+                + ", engine=" + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car clonedCar = (Car) super.clone();
+        Engine clonedEngine = new Engine();
+        clonedCar.setEngine(clonedEngine);
+        return clonedCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,88 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable{
+    private static long idCounter = 0;
+    private long id;
+    private String fuel;
+    private int productionYear;
+    private String company;
+    private double fuelConsumption;
+    private int revolutionsPerMinute;
 
+    public Engine(String fuel,
+                  int productionYear,
+                  String company,
+                  double fuelConsumption,
+                  int revolutionsPerMinute) {
+        this.id = idCounter++;
+        this.fuel = fuel;
+        this.productionYear = productionYear;
+        this.company = company;
+        this.fuelConsumption = fuelConsumption;
+        this.revolutionsPerMinute = revolutionsPerMinute;
+    }
+
+    public Engine() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public int getRevolutionsPerMinute() {
+        return revolutionsPerMinute;
+    }
+
+    public void setRevolutionsPerMinute(int revolutionsPerMinute) {
+        this.revolutionsPerMinute = revolutionsPerMinute;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "id=" + id
+                + ", fuel='" + fuel + '\''
+                + ", productionYear=" + productionYear
+                + ", company='" + company + '\''
+                + ", fuelConsumption=" + fuelConsumption
+                + ", revolutionsPerMinute=" + revolutionsPerMinute
+                + '}';
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
