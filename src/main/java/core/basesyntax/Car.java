2fde97daa432b30305c09580cecd166c9061ebb2
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-07-31 15:15:38 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String brand;
+    private String transmission;
+    private int yearOfManufacture;
+    private int numberOfSeats;
+    private int price;
+    private Engine engine;
 
+    public Car(String brand, String transmission, int yearOfManufacture,
+               int numberOfSeats, int price, Engine engine) {
+        this.brand = brand;
+        this.transmission = transmission;
+        this.yearOfManufacture = yearOfManufacture;
+        this.numberOfSeats = numberOfSeats;
+        this.price = price;
+        this.engine = engine;
+    }
+
+    public Car clone() {
+        return new Car(brand, transmission, yearOfManufacture, numberOfSeats, price, engine);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int numberOfCylinders;
+    private double displacement;
+    private int horsePower;
+    private int torque;
+    private String engineType;
 
+    public Engine(int numberOfCylinders, double displacement, int horsePower,
+                  int torque, String engineType) {
+        this.numberOfCylinders = numberOfCylinders;
+        this.displacement = displacement;
+        this.horsePower = horsePower;
+        this.torque = torque;
+        this.engineType = engineType;
+    }
+
+    public Engine clone() {
+        return new Engine(numberOfCylinders, displacement, horsePower, torque, engineType);
+    }
 }
