e7a14b6d5a841458e2888b5cfd6cbcd74f8098fb
ROMASHSX <andriiromash@gmail.com>
2020-07-31 12:39:48 +0300

    Task is done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,37 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String modelName;
+    private String color;
+    private Engine engine;
+    private String type;
+    private int numbersOfPassengers;
 
+    public Car(String modelName,
+               String color,
+               Engine engine,
+               String type,
+               int numbersOfPassengers) {
+        this.modelName = modelName;
+        this.color = color;
+        this.engine = engine;
+        this.type = type;
+        this.numbersOfPassengers = numbersOfPassengers;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car) super.clone();
+        Engine engine = this.getEngine().clone();
+        newCar.setEngine(engine);
+        return newCar;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,26 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String modelName;
+    private String type;
+    private double displacement;
+    private double maximumPowerOutput;
+    private int numberOfCylinder;
 
+    public Engine(String modelName,
+                  String type,
+                  double displacement,
+                  double maximumPowerOutput,
+                  int numberOfCylinder) {
+        this.modelName = modelName;
+        this.type = type;
+        this.displacement = displacement;
+        this.maximumPowerOutput = maximumPowerOutput;
+        this.numberOfCylinder = numberOfCylinder;
+    }
+
+    @Override
+    protected Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
