bac01af726a86ab76f5a4038c391c45c3d672aaf
Marina <maryna.voloshyna.11@gmail.com>
2020-07-31 17:36:06 +0300

    add soluyion

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String colour;
+    private String model;
+    private int weight;
+    private Engine engine;
+    private int numberOfSits;
 
+    public Car(String colour, String model, int weight, Engine engine, int numberOfSits) {
+        this.colour = colour;
+        this.model = model;
+        this.weight = weight;
+        this.engine = engine;
+        this.numberOfSits = numberOfSits;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car car = new Car(colour, model, weight, engine.clone(), numberOfSits);
+        return car;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int cylinderDiameter;
+    private int pistonStroke;
+    private int crankRadius;
+    private double engineDisplacement;
+    private int enginePower;
 
+    public Engine(int cylinderDiameter, int pistonStroke, int crankRadius,
+                  double engineDisplacement, int enginePower) {
+        this.cylinderDiameter = cylinderDiameter;
+        this.pistonStroke = pistonStroke;
+        this.crankRadius = crankRadius;
+        this.engineDisplacement = engineDisplacement;
+        this.enginePower = enginePower;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(cylinderDiameter, pistonStroke, crankRadius,
+                engineDisplacement, enginePower);
+    }
 }
