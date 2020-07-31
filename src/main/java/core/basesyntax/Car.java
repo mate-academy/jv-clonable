56f30c48f517b4e73f9a9124174571a4a3936b25
Oleksii Tromsa <tromsaalex@gmail.com>
2020-07-31 15:50:14 +0300

    Add solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private int yearOfProduction;
+    private Engine engine;
+    private int mileage;
 
+    public Car(String model, String color, int yearOfProduction, Engine engine, int mileage) {
+        this.model = model;
+        this.color = color;
+        this.yearOfProduction = yearOfProduction;
+        this.engine = engine;
+        this.mileage = mileage;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        color = color;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car newCar = (Car) super.clone();
+        newCar.engine = (Engine) engine.clone();
+
+        return newCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private double capacity;
+    private String powerType;
+    private int numberOfCylinders;
+    private int fuelConsumption;
 
+    public Engine(int power, double capacity, String powerType, int numberOfCylinders,
+                  int fuelConsumption) {
+        this.power = power;
+        this.capacity = capacity;
+        this.powerType = powerType;
+        this.numberOfCylinders = numberOfCylinders;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getPowerType() {
+        return powerType;
+    }
+
+    public void setPowerType(String powerType) {
+        this.powerType = powerType;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
 }
