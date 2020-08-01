9304af953ed8427044520aa78cf02881baf41929
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-01 09:19:13 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private Engine engine;
+    private String color;
+    private int doorCount;
+    private int issueYear;
 
+    public Car(String name, Engine engine, String color, int doorCount, int issueYear) {
+        this.name = name;
+        this.engine = engine;
+        this.color = color;
+        this.doorCount = doorCount;
+        this.issueYear = issueYear;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Engine cloneEngine = this.engine;
+        Car cloneCar = new Car(this.name, cloneEngine, this.color, this.doorCount, this.issueYear);
+        return cloneCar;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private int combustionChambersVolume;
+    private int power;
+    private int torque;
+    private int fuelConsumption;
 
+    public Engine(String name, int combustionChambersVolume,
+                  int power, int torque, int fuelConsumption) {
+        this.name = name;
+        this.combustionChambersVolume = combustionChambersVolume;
+        this.power = power;
+        this.torque = torque;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return (Engine) super.clone();
+    }
 }
