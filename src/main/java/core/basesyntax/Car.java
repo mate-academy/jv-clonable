dd53dc7938573e74bdfe4443259ae450f0fb4fac
OlessiaKharchenko <olessia.kharchenko@gmail.com>
2020-08-01 08:16:19 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String color;
+    private String brand;
+    private int price;
+    private int weight;
+    private Engine engine;
 
+    public Car(String color, String brand, int price, int weight, Engine engine) {
+        this.color = color;
+        this.brand = brand;
+        this.price = price;
+        this.weight = weight;
+        this.engine = engine;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(color, brand, price, weight, engine.clone());
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int capacity;
+    private int power;
+    private int speed;
+    private int weight;
+
+    private Engine(String type, int capacity, int power, int speed, int weight) {
+        this.type = type;
+        this.capacity = capacity;
+        this.power = power;
+        this.speed = speed;
+        this.weight = weight;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(type, capacity, power, speed, weight);
+    }
 
 }
