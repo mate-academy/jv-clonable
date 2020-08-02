c609010c22614f8b8bdd3ba5bf938dc7e1588b0d
vosker <nikita.vaganov16@gmail.com>
2020-08-02 17:17:21 +0300

    wrong variables declaration fixed

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -4,18 +4,18 @@ public class Engine implements Cloneable {
     private String manufacturer;
     private String name;
     private int age;
-    private int cylinder_number;
+    private int cylinderNumber;
     private int weight;
 
-    public Engine(String manufacturer, String name, int age, int cylinder_number, int weight) {
+    public Engine(String manufacturer, String name, int age, int cylinderNumber, int weight) {
         this.manufacturer = manufacturer;
         this.name = name;
         this.age = age;
-        this.cylinder_number = cylinder_number;
+        this.cylinderNumber = cylinderNumber;
         this.weight = weight;
     }
 
     public Engine clone() throws CloneNotSupportedException {
-        return new Engine(manufacturer, name, age, cylinder_number, weight);
+        return new Engine(manufacturer, name, age, cylinderNumber, weight);
     }
 }
