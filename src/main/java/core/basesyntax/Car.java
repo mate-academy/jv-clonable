6579acae2c6bfdf412bc558a42a7de386c6631b5
Dubovaya <dubovaya.java@gmail.com>
2020-08-02 19:42:04 +0300

    leraHW-5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String model;
+    private int mph;
+    private int capacity;
+    private int range;
 
+    public Car(Engine engine, String model, int mph, int capacity, int range) {
+        this.engine = engine;
+        this.model = model;
+        this.mph = mph;
+        this.capacity = capacity;
+        this.range = range;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(engine.clone(), model, mph, capacity, range);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private int displacement;
+    private int cylinder;
+    private int torque;
+    private String type;
 
+    public Engine(int power, int displacement,
+                  int cylinder, int torque, String type) {
+        this.power = power;
+        this.displacement = displacement;
+        this.cylinder = cylinder;
+        this.torque = torque;
+        this.type = type;
+    }
+
+    @Override
+    protected Engine clone() {
+        return new Engine(power, displacement, cylinder, torque, type);
+    }
 }
