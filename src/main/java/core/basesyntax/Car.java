d313428779bb2222302abbdad1c596fbaf798d48
vitalka1994 <59143874+vitalka1994@users.noreply.github.com>
2020-08-02 13:55:26 +0300

    HW-5-1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String model;
+    private String color;
+    private int age;
+    private int weight;
+    private int price;
 
+    public Car(Engine engine, String model, String color, int age, int weight, int price) {
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        Car car1 = new Car(engine, model, color, age, weight, price);
+        return car1;
+    }
 }
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,18 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String type;
+    private int horsePover;
+    private int age;
+    private double volume;
+    private double consumption;
 
+    public Engine(String type, int horsePover, int age, double volume, double consumption) {
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        Engine engine1 = new Engine(type, horsePover, age, volume, consumption);
+        return engine1;
+    }
 }
