40ecddced641f37605e3bbee5ab2c7b114dfc49d
Jah <moskovienkor@gmail.com>
2020-08-02 17:23:48 +0300

    HOMEWORK-5-1: - done;

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String name;
+    private String model;
+    private int year;
+    private int id;
+    private boolean isNew;
 
+    public Car(String name, String model, int year, int id, boolean isNew) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.id = id;
+        this.isNew = isNew;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(name, model, year, id, isNew);
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String name;
+    private String model;
+    private int year;
+    private int id;
+    private boolean isNew;
 
+    public Engine(String name, String model, int year, int id, boolean isNew) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.id = id;
+        this.isNew = isNew;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(name, model, year, id, isNew);
+    }
 }
