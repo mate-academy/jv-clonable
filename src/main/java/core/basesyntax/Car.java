b61de34009273df0506386e5337113ac360d0a39
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-03 12:58:22 +0300

    hw

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,24 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+
+    private String model;
+    private String color;
+    private int velocity;
+    private Engine engine;
+    private double timeFrom0To100;
+
+    public Car(String model, String color, int velocity, Engine engine, double timeFrom0To100) {
+        this.model = model;
+        this.color = color;
+        this.velocity = velocity;
+        this.engine = engine;
+        this.timeFrom0To100 = timeFrom0To100;
+    }
+
+    @Override
+    public Car clone() {
+        return new Car(model, color, velocity, engine.clone(), timeFrom0To100);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,23 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private String model;
+    private String type;
+    private int power;
+    private int frequency;
+    private int weight;
+
+    public Engine(String model, String type, int power, int frequency, int weight) {
+        this.model = model;
+        this.type = type;
+        this.power = power;
+        this.frequency = frequency;
+        this.weight = weight;
+    }
+
+    @Override
+    public Engine clone() {
+        return new Engine(model, type, power, frequency, weight);
+    }
 }
