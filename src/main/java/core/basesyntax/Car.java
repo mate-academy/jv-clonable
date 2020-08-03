4a6e2a7fb4c5d67a78f0eadbc9cf9d01d44ab166
KhOksana99 <kharabara99@gmail.com>
2020-08-03 14:36:38 +0300

    homeWork5.1

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,63 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private int yearOfMade;
+    private String model;
+    private String producer;
+    private int price;
+
+    Car(Engine engine, int yearOfMade, String model, String producer, int price) {
+        this.engine = engine;
+        this.yearOfMade = yearOfMade;
+        this.model = model;
+        this.producer = producer;
+        this.price = price;
+    }
+
+    Engine getEngine() {
+        return engine;
+    }
+
+    void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getYearOfMade() {
+        return yearOfMade;
+    }
+
+    public void setYearOfMade(int yearOfMade) {
+        this.yearOfMade = yearOfMade;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public Car clone() throws CloneNotSupportedException {
+        return new Car(engine.clone(), yearOfMade, model, producer, price);
+    }
 
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,62 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int countOfHorsePower;
+    private double volume;
+    private int countOfValves;
+    private String type;
+    private int yearOfMade;
 
+    Engine(int countOfHorsePower, double volume, int countOfValves, String type, int yearOfMade) {
+        this.countOfHorsePower = countOfHorsePower;
+        this.volume = volume;
+        this.countOfValves = countOfValves;
+        this.type = type;
+        this.yearOfMade = yearOfMade;
+    }
+
+    public int getCountOfHorsePower() {
+        return countOfHorsePower;
+    }
+
+    public void setCountOfHorsePower(int countOfHorsePower) {
+        this.countOfHorsePower = countOfHorsePower;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public int getCountOfValves() {
+        return countOfValves;
+    }
+
+    public void setCountOfValves(int countOfValves) {
+        this.countOfValves = countOfValves;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYearOfMade() {
+        return yearOfMade;
+    }
+
+    public void setYearOfMade(int yearOfMade) {
+        this.yearOfMade = yearOfMade;
+    }
+
+    @Override
+    public Engine clone() throws CloneNotSupportedException {
+        return new Engine(countOfHorsePower, volume, countOfValves, type, yearOfMade);
+    }
 }
