f6c69840a58f915718a55574bf5cd1967490f09e
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-04 14:08:55 +0200

    done HW

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine("predator","OHV", "Germany", "gas", 500);
+        Car audi = new Car("Audi", "red", 2500, 2010);
+        audi.setEngine(engine);
+        Car carClone = audi.clone();
+        System.out.println(carClone.toString());
+        audi.getEngine().setOrigin("Italy");
+        System.out.println(carClone.toString());
+        System.out.println(audi.toString());
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,78 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private Engine engine;
+    private int weight;
+    private int year;
 
+    public Car(String name, String color, int weight, int year) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "name='" + name + '\''
+                + ", color='" + color + '\''
+                + ", engine=" + engine
+                + ", weight=" + weight
+                + ", year=" + year
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car car = (Car) super.clone();
+            car.setEngine(engine.clone());
+            return car;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone a car", e);
+        }
+    }
 }
