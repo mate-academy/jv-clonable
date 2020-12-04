73547c1b94de0b9a14857f13313ddad681120b7f
nikelok01@gmail.com <163amefeZ>
2020-12-04 21:09:25 +0200

    -created getters, setters and Constructors for classes Engine and Car; - Implement Cloneable for classes Engine, Car;

--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,80 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean turbocharged;
+    private double engineCapacity;
+    private int horsePower;
+    private String engineConfiguration;
+    private int valueCylinder;
 
+    public Engine(boolean turbocharged, double engineCapacity, int horsePower,
+                  String engineConfiguration, int valueCylinder) {
+        this.turbocharged = turbocharged;
+        this.engineCapacity = engineCapacity;
+        this.horsePower = horsePower;
+        this.engineConfiguration = engineConfiguration;
+        this.valueCylinder = valueCylinder;
+    }
+
+    public boolean isTurbocharged() {
+        return turbocharged;
+    }
+
+    public double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getEngineConfiguration() {
+        return engineConfiguration;
+    }
+
+    public int getValueCylinder() {
+        return valueCylinder;
+    }
+
+    public void setTurbocharged(boolean turbocharged) {
+        this.turbocharged = turbocharged;
+    }
+
+    public void setEngineCapacity(double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setEngineConfiguration(String engineConfiguration) {
+        this.engineConfiguration = engineConfiguration;
+    }
+
+    public void setValueCylinder(int valueCylinder) {
+        this.valueCylinder = valueCylinder;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "turbocharged=" + turbocharged
+                + ", engineCapacity=" + engineCapacity
+                + ", horsePower=" + horsePower
+                + ", engineConfiguration='" + engineConfiguration + '\''
+                + ", valueCylinder=" + valueCylinder
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Engine clone!");
+        }
+    }
 }
+
+
