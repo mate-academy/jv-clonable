788323695e6a86e5fe7150d35f71173eae200296
Okulov Vadim <Vedroidua@gmail.com>
2020-12-04 17:38:49 +0200

    Task completed

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine("VAG", "2.0 TDI BlueMotion", "CKFC",
+                "Euro4", 150, 2.0);
+        Car car = new Car("Volkswagen", "Golf Variant Mk7", "Wagon",
+                "Front-engine, front-wheel-drive", 216, engine);
+        Car carClone = car.clone();
+        car.getEngine().setToxicEmission("Euro5");
+        System.out.println(car);
+        System.out.println(carClone);
     }
 }
