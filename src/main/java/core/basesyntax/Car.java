21d91b110af0e2f8367e0aa4b1e98dc0c9444f5e
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-05 16:33:41 +0200

    cloneable implementation

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,79 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private int year;
+    private int maxSpeed;
+    private Engine engine;
 
+    public Car(String model, String color, int year, int maxSpeed, Engine engine) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.maxSpeed = maxSpeed;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "model='" + model + '\''
+                + ", color='" + color + '\''
+                + ", year=" + year
+                + ", maxSpeed=" + maxSpeed
+                + ", engine=" + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clone = (Car) super.clone();
+            clone.setEngine(engine.clone());
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Car");
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,77 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private int power;
+    private int lifeTime;
+    private boolean isStarted;
+    private boolean isStopped;
 
+    public Engine(String model, int power, int lifeTime, boolean isStarted, boolean isStopped) {
+        this.model = model;
+        this.power = power;
+        this.lifeTime = lifeTime;
+        this.isStarted = isStarted;
+        this.isStopped = isStopped;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getLifeTime() {
+        return lifeTime;
+    }
+
+    public void setLifeTime(int lifeTime) {
+        this.lifeTime = lifeTime;
+    }
+
+    public boolean isStarted() {
+        return isStarted;
+    }
+
+    public void setStarted(boolean started) {
+        isStarted = started;
+    }
+
+    public boolean isStopped() {
+        return isStopped;
+    }
+
+    public void setStopped(boolean stopped) {
+        isStopped = stopped;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "model='" + model + '\''
+                + ", power=" + power
+                + ", lifeTime=" + lifeTime
+                + ", isStarted=" + isStarted
+                + ", isStopped=" + isStopped
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine");
+        }
+    }
 }
