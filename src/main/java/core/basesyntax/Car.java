3492ec42c8d376c6efefed2fb64ddb7e3f217994
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-05 13:13:45 +0200

    Refer to homework cloneable
    wrote Car.java
    wrote Engine.java
    
    Features
    
     - rewrite toString
     - rewrite clone

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,75 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String model;
+    private int coast;
+    private int doors;
+    private boolean autoTransmission = false;
 
+    Car(Engine engine, String model, int coast, String color) {
+        this.engine = engine;
+        this.model = model;
+        this.coast = coast;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isAutoTransmission() {
+        return autoTransmission;
+    }
+
+    public void setAutoTransmission(boolean autoTransmission) {
+        this.autoTransmission = autoTransmission;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCoast() {
+        return coast;
+    }
+
+    public void setCoast(int coast) {
+        this.coast = coast;
+    }
+
+
+    @Override
+    public Car clone() {
+        Car clonedCar = null;
+        try {
+            clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create cat clone", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car model is " + getModel() +
+                "! It would be cost less " + coast + "$ just for you buddy. "
+                +" Car has " + engine.toString();
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,69 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private int power;
+    private double galPerMile;
+    private double volume;
+    private double torque;
 
+    Engine(String model, int power){
+        this.model = model;
+        this.power = power;
+    }
+
+    public double getGalPerMile() {
+        return galPerMile;
+    }
+
+    public void setGalPerMile(double galPerMile) {
+        this.galPerMile = galPerMile;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getTorque() {
+        return torque;
+    }
+
+    public void setTorque(double torque) {
+        this.torque = torque;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    protected Engine clone()  {
+        try {
+            return (Engine)super.clone();
+        } catch (CloneNotSupportedException e) {
+           throw new RuntimeException("Can't clone engine", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Engine model is " + model
+                + ", power is " + power;
+    }
 }
