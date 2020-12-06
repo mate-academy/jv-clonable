7cf1c49d9a9a950ab228c59b407a740bbe06b77f
Roman Saviak <saviak.roman@gmail.com>
2020-12-06 18:56:49 +0200

    Made clone of car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int year;
+    private String model;
+    private String colour;
+    private String type;
+    private String tyres;
+    private Engine engine;
 
+    public Car(int year, String model, String colour, String type, String tyres) {
+        this.year = year;
+        this.model = model;
+        this.colour = colour;
+        this.type = type;
+        this.tyres = tyres;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car: Model " + model + "; Year " + year + "; Colour " + colour + "; Type " + type
+                + "; Tyres " + tyres + "; " + engine;
+    }
+
+    @Override
+    protected Car clone() {
+        try {
+            Car cloneCar = (Car) super.clone();
+            cloneCar.setEngine(engine.clone());
+            return cloneCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,65 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int year;
+    private String model;
+    private int numberOfCylinder;
+    private double displacement;
+    private int enginePower;
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumberOfCylinder() {
+        return numberOfCylinder;
+    }
+
+    public void setNumberOfCylinder(int numberOfCylinder) {
+        this.numberOfCylinder = numberOfCylinder;
+    }
+
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine: Model " + model + "; Year " + year + "; Number of cylinder "
+                + numberOfCylinder + "; Displacement " + displacement
+                + "; Engine power " + enginePower;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can create Engine clone", e);
+        }
+    }
 }
