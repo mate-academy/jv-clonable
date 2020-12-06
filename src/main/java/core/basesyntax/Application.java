31dda3b4c457786fd23b8106106502cf335863fa
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-06 18:48:03 +0200

    Solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,20 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine();
+        engine.setPower(200);
 
+        Car car = new Car();
+        car.setModel("audi");
+        car.setColor("red");
+        car.setEngine(engine);
+
+        System.out.println(car);
+
+        Car carClone = car.clone();
+        car.getEngine().setPower(300);
+
+        System.out.println(carClone);
+        System.out.println(car);
     }
 }
