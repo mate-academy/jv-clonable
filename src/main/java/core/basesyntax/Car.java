dc73e0b1b3be6e2eb3c7e1643a1db802bb634930
Vitalik Kopach <apple_air@ukr.net>
2020-12-07 02:11:51 +0200

    implemented  method clone() in classes Car and Engine

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,72 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
 
+    private Engine engine;
+    private String name;
+    private String color;
+    private int price;
+    private int year;
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+               + "engine=" + engine
+               + ", name='" + name + '\''
+               + ", color='" + color + '\''
+               + ", price=" + price
+               + ", year=" + year
+               + '}';
+    }
+
+    @Override
+    protected Car clone() {
+        try {
+            Car cloneCar = (Car) super.clone();
+            cloneCar.setEngine(engine.clone());
+            return cloneCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone: " + e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,69 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
 
+    private String type;
+    private int volume;
+    private int weight;
+    private int speed;
+    private int power;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+               + "type='" + type + '\''
+               + ", volume=" + volume
+               + ", weight=" + weight
+               + ", speed=" + speed
+               + '}';
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Engine clone: " + e);
+        }
+    }
 }
