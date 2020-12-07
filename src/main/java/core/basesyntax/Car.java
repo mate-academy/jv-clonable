6e56c57752530959b1bd557376f625cef95de848
Likh-Alex <likhachov991@protonmail.com>
2020-12-07 14:07:28 +0200

    Implemented cloning in Car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,59 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String maker;
+    private String model;
+    private Engine engine;
+    private String color;
+    private int seats;
 
+    public Car(String maker, String model, Engine engine, String color, int seats) {
+        this.maker = maker;
+        this.model = model;
+        this.engine = engine;
+        this.color = color;
+        this.seats = seats;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "maker='" + maker + '\''
+                + ", model='" + model + '\''
+                + ", engine=" + engine
+                + ", color='" + color + '\''
+                + ", seats=" + seats
+                + '}';
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can not create car clone", e);
+        }
+    }
 }
