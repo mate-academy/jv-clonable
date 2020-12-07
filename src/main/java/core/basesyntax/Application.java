ab3612c78e7d24222c508a8a9791dd887084178b
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-07 13:08:50 +0200

    firest commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,21 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
 
+        Engine engine = new Engine();
+        engine.setModel("GTS");
+        engine.setHorsePower(240);
+        engine.setEngineValue(3.0);
+        engine.setVariatorType(12);
+        engine.setFuelType("gaz");
+
+        Car car = new Car();
+        car.setModel("Dodge");
+        car.setEngine(engine);
+        car.setEstablishYear(2019);
+        car.setVehicleType("coupe");
+        car.setPassengersCapacity(2);
+
+        Car clonedCar = car.clone();
     }
 }
