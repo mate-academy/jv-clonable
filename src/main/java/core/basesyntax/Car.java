b04d57073d7e6c272805f684ef7bb9667309498f
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-07 12:49:55 +0200

    clone() method implemented, classes Car and Engine completed.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,60 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private int year;
+    private Engine engine;
+    private String color;
+    private int maxSpeed;
 
+    public Car(String model, int year, Engine engine, String color, int maxSpeed) {
+        this.model = model;
+        this.year = year;
+        this.engine = engine;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("This car can't be cloned", e);
+        }
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("Model: ");
+        stringBuilder.append(model).append("\nYear: ").append(year).append("\n")
+                .append(engine.toString()).append("\nColor: ").append(color)
+                .append("\nMax Speed: ").append(maxSpeed);
+        return stringBuilder.toString();
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double volume;
+    private int year;
+    private int weight;
+    private String madeBy;
+    private boolean turbo;
 
+    public Engine(double volume, int year, int weight, String madeBy, boolean turbo) {
+        this.volume = volume;
+        this.year = year;
+        this. weight = weight;
+        this.madeBy = madeBy;
+        this.turbo = turbo;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Engine can't be cloned", e);
+        }
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("Engine:\\Vol: ");
+        stringBuilder.append(volume).append("\\Year: ").append(year).append("\\Weight: ")
+                .append(weight).append("\\Made BY: ").append(madeBy)
+                .append("\\Has turbo: ").append(turbo);
+        return stringBuilder.toString();
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setMadeBy(String madeBy) {
+        this.madeBy = madeBy;
+    }
+
+    public void setTurbo(boolean turbo) {
+        this.turbo = turbo;
+    }
 }
