4caac7ede3721b0434cff7495a0a379fbfc5b198
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-07 17:05:03 +0200

    Add 5 fields to each class override `toString()` method for the car and engine override clone() method for car and engine all works

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,18 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine mazdaEngine = new Engine("mazda", "v12", "diesel", 350, 25);
+        Car mazda = new Car("mazda", "cv-7", "silver", 5);
+        mazda.setEngine(mazdaEngine);
+        Car mazdaClone = mazda.clone();
+        System.out.println(mazda);
+        System.out.println(mazdaClone);
+        mazdaClone.getEngine().setType("Electric");
+        mazdaClone.getEngine().setHorsePower(250);
+        mazdaClone.getEngine().setModel("Tesla model S engine");
+        mazdaClone.getEngine().setFuelConsumption(10);
+        mazdaClone.getEngine().setName("Electric trolleybus!");
+        System.out.println(mazda);
+        System.out.println(mazdaClone);
     }
 }
