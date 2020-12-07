33664dc9ab5633df17768c3a28acaa22519fcf9d
Igor Bochkor <igorbochkor@gmail.com>
2020-12-07 15:03:47 +0200

    added solution clonable

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,71 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private String year;
+    private Engine engine;
+    private int maxSpeed;
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "model='" + model + '\''
+                + ", color='" + color + '\''
+                + ", year='" + year + '\''
+                + ", engine=" + engine
+                + ", maxSpeed=" + maxSpeed
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car carClone = (Car) super.clone();
+            carClone.setEngine(engine.clone());
+            return carClone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create clone ", e);
+        }
+    }
 }
