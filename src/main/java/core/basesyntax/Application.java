afc259c3f03083b98f723e520b953a41895ccb99
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-08 11:13:28 +0100

    clone method defined in classes Car and Engine.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,25 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine();
+        engine.setForm("round");
+        engine.setGasOrDiesel("gas");
+        engine.setName("bestEngine");
+        engine.setPower(350);
+        engine.setVolume(200);
 
+        Car myCar = new Car();
+        myCar.setColor("red");
+        myCar.setModel("BMW");
+        myCar.setSeats(2);
+        myCar.setSpeed(400);
+        myCar.setYearOfConstruction(2020);
+        myCar.setEngine(engine);
+
+        Car clonedMyCar = myCar.clone();
+        myCar.getEngine().setGasOrDiesel("diesel");
+
+        System.out.println(clonedMyCar);
+        System.out.println(myCar);
     }
 }
