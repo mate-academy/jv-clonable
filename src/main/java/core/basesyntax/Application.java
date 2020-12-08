ad82d5adc78e5d405aca4a3a09cc46bfdd0f4bb4
tbondare <tanya.bon2016@gmail.com>
2020-12-08 17:40:39 +0200

    Solution was added firstly

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,22 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine();
+        engine.setFuelsType("electric");
+        Car bmw = new Car();
 
+        bmw.setColor("red");
+        bmw.setSpeed(200);
+        bmw.setEngine(engine);
+        bmw.toString();
+        System.out.println(bmw);
+
+        Car clonedCar = bmw.clone();
+        System.out.println(clonedCar);
+
+        bmw.getEngine().setFuelsType("firewood");
+        bmw.setColor("white");
+        bmw.setSpeed(180);
+        System.out.println(bmw);
     }
 }
