8be0998e94813183c6a86e5a3cff84e25a946fe2
AndreiYazadzhi <belator251000@gmail.com>
2020-12-08 12:21:24 +0200

    Fix object Engine

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Engine engine = new Engine("JZ-1", 600, 550, 9.5, 7);
-        Engine engine2 = new Engine("JZ-2", 700, 550, 12.5, 6);
         Car toyota = new Car("Toyota", "Red", engine, 220, 1800);
         Car clonedToyota = toyota.clone();
-        clonedToyota.setEngine(engine2);
+        clonedToyota.setEngine(new Engine("JZ-2", 700, 550, 12.5, 6));
         System.out.println(toyota.toString());
         System.out.println(clonedToyota.toString());
     }
