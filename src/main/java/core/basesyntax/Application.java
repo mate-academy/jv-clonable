392e8ec07f2f99b4005d90eb9faf635e8c2e1fab
Alekearl <alexandrhurko@gmail.com>
2020-12-08 12:37:39 +0200

    refactored class Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,8 +1,22 @@
 package core.basesyntax;
 
 public class Application {
-    public static void main(String[] args) {
-        // test your code here ...
 
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+        engine.setEngineModel("ZX101");
+        engine.setHorsePower(300);
+
+        Car car = new Car();
+        car.setCarName("Audi");
+        car.setCarModel("A4");
+        car.setCarBody("sedan");
+        car.setEngine(engine);
+
+        Car carClone = car.clone();
+        car.getEngine().setEngineModel("XX303");
+        car.getEngine().setHorsePower(300);
+        System.out.println(carClone);
+        System.out.println(car);
     }
 }
