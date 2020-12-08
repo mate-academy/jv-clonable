e97b89df089a0acab883ca773bcfb3ae00b46a18
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-08 14:44:34 +0200

    1) Commit

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,25 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine();
+        engine.setCapacity(3);
+        engine.setFuelType("Gasoline");
+        engine.setNumber(15262367);
+        engine.setType("V8");
+        engine.setSize("Monstro");
 
+        Car car = new Car();
+        car.setColor("Black");
+        car.setCountry("Germany");
+        car.setEngine(engine);
+        car.setYearMade(2015);
+        car.setName("AUDI");
+
+        System.out.println(car);
+        System.out.println(engine);
+
+        Car clonedCar = car.clone();
+        clonedCar.getEngine().setCapacity(4);
+        System.out.println(clonedCar);
     }
 }
