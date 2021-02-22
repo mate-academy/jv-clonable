86be1e42fe7411ba2a8590f9810cf4179ad0f766
Tetiana Chupika <mtv1301@gmail.com>
2021-02-22 17:06:15 +0200

    added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,21 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine();
+        engine.setColor("red");
+
+        Car car = new Car();
+        car.setModel("MT");
+        car.setColor("red");
+        car.setHasEngine(true);
+        car.setEngine(engine);
+        System.out.println(car);
+
+        Car cloneCar = car.clone();
+        System.out.println(cloneCar);
+        car.getEngine().setColor("black");
+        System.out.println(cloneCar);
+        System.out.println(car);
 
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,81 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private int number;
+    private boolean hasEngine;
+    private String color;
+    private int year;
+    private Engine engine;
 
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isHasEngine() {
+        return hasEngine;
+    }
+
+    public void setHasEngine(boolean hasEngine) {
+        this.hasEngine = hasEngine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {"
+                + "model = '" + model + '\''
+                + ", number = " + number
+                + ", hasEngine = " + hasEngine
+                + ", color = '" + color + '\''
+                + ", year = " + year
+                + ", engine = " + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car cloneCar = (Car) super.clone();
+            cloneCar.setEngine(engine.clone());
+            return cloneCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create car clone", e);
+        }
+    }
 }
