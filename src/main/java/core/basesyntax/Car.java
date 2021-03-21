3ded337d8d5d7256b8056468a5a4f4e64429390c
vsak <vsak@itsmartflex.com>
2021-03-21 18:05:02 +0200

    Solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,67 @@
 package core.basesyntax;
 
-public class Car {
+public class Car extends Engine implements Cloneable {
+    private String color;
+    private String model;
+    private String owner;
+    private int year;
+    private Engine engine;
 
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public Car clone() {
+        Car clonedCar = (Car) super.clone();
+        clonedCar.setEngine(engine.clone());
+        return clonedCar;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "color='" + color + '\''
+                + "model='" + model + '\''
+                + "engine='" + engine + '\''
+                + "owner='" + owner + '\''
+                + "year='" + year + '\''
+                + "}";
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,69 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int power;
+    private String model;
+    private boolean wasRepaired;
+    private int turbineCount;
+    private String whoDevelop;
 
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isWasRepaired() {
+        return wasRepaired;
+    }
+
+    public void setWasRepaired(boolean wasRepaired) {
+        this.wasRepaired = wasRepaired;
+    }
+
+    public int getTurbineCount() {
+        return turbineCount;
+    }
+
+    public void setTurbineCount(int turbineCount) {
+        this.turbineCount = turbineCount;
+    }
+
+    public String getWhoDevelop() {
+        return whoDevelop;
+    }
+
+    public void setWhoDevelop(String whoDevelop) {
+        this.whoDevelop = whoDevelop;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine class", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "power='" + power + '\''
+                + "model='" + model + '\''
+                + "turbineCount='" + turbineCount + '\''
+                + "whoDevelop='" + whoDevelop + '\''
+                + "wasRepaired='" + wasRepaired + '\''
+                + "}";
+    }
 }
