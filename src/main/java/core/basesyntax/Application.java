cf558922dbae207db776e2f4f6caec25923e5447
Maksim <max.freedman.main@gmail.com>
2021-03-31 17:12:23 +0300

    added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,26 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine(
+                "№123456789",
+                "model", 1000,
+                3.5,
+                250);
+        Car car = new Car(
+                "Mercedes",
+                "Black",
+                "Benz&Cie",
+                2000, engine);
 
+        Engine newEngine = new Engine();
+        newEngine.setEngineVolume(4);
+        newEngine.setHorsePower(300);
+        newEngine.setMileage(500);
+
+        Car clonedCar = car.clone();
+        car.setEngine(newEngine);
+
+        System.out.println(car);
+        System.out.println(clonedCar);
     }
 }
