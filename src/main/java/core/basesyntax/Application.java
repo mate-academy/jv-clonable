d65cd52b104e8d04e5cc0bf47ab95ae4fe6567b4
dant0n <dmibuc@gmail.com>
2021-04-01 18:25:43 +0300

    Finish homework requirements.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine("OriginalEngineType", 1, 2, 3, 4);
+        Car car = new Car("Audi", "A6", 2007, 70, engine);
+        Car clonedCar = car.clone();
+        car.getEngine().setType("NewEngineType");
+        System.out.println(car);
+        System.out.println(clonedCar);
     }
 }
