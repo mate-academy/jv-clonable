bddcc7ee49d63612eb9ed7ccbda17522787e1d90
KseniaObertii <ksenia.obertii@users.noreply.github.com>
2021-04-02 17:15:08 +0300

    "added solution"

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private int number;
+    private Engine engine;
+    private String color;
+    private int gasolineVolume;
 
+    public Car(String model, int number, Engine engine, String color, int gasolineVolume) {
+        this.model = model;
+        this.number = number;
+        this.engine = engine;
+        this.color = color;
+        this.gasolineVolume = gasolineVolume;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Car!", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{model='" + model + '\'' + ", number=" + number
+                + ", engine=" + engine + ", color='" + color + '\''
+                + ", gasolineVolume=" + gasolineVolume + '}';
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,38 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean crankshaft;
+    private boolean cylinderHead;
+    private boolean camshaft;
+    private boolean valves;
+    private boolean oilPan;
 
+    public Engine(boolean crankshaft, boolean cylinderHead,
+                  boolean camshaft, boolean valves, boolean oilPan) {
+        this.crankshaft = crankshaft;
+        this.cylinderHead = cylinderHead;
+        this.camshaft = camshaft;
+        this.valves = valves;
+        this.oilPan = oilPan;
+    }
+
+    public void setCrankshaft(boolean crankshaft) {
+        this.crankshaft = crankshaft;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine!", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{crankshaft='" + crankshaft + '\'' + ", cylinderHead='" + cylinderHead
+                + '\'' + ", camshaft='" + camshaft + '\'' + ", valves='" + valves
+                + '\'' + ", oilPan='" + oilPan + '\'' + '}';
+    }
 }
