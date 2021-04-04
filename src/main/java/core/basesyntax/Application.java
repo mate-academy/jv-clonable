066997798e28d2a92750fa995511cdd15309591e
Vlad-Andr <andreev.v99@gmail.com>
2021-04-04 20:06:18 +0300

    added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine("A1", "pure3", 4, 15.0, 22.0);
+        Car car = new Car("BMW", "white", "4x4", 22500.0, engine);
+        Car carClone = car.clone();
+        car.getEngine().setNumberOfCylinders(6);
+        System.out.println("Car: " + car);
+        System.out.println("Cloned car: " + carClone);
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,78 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String version;
+    private int numberOfCylinders;
+    private double volume;
+    private double fuelConsumption;
 
+    public Engine(String model, String version, int numberOfCylinders,
+                  double volume, double fuelConsumption) {
+        this.model = model;
+        this.version = version;
+        this.numberOfCylinders = numberOfCylinders;
+        this.volume = volume;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "model='" + model + '\''
+                + ", version='" + version + '\''
+                + ", numberOfCylinders=" + numberOfCylinders
+                + ", volume=" + volume
+                + ", fuelConsumption=" + fuelConsumption
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone engine", e);
+        }
+    }
 }
