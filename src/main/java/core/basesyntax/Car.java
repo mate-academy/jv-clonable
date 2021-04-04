93b16a6a80642ae0fa81687c91462c45a84b929c
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-04 19:17:55 +0300

    implement Car, Engine classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,59 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private int doors;
+    private int wheels;
+    private String name;
+    private String model;
+    private Engine engine;
 
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "doors="
+                + doors
+                + ", wheels="
+                + wheels
+                + ", name='"
+                + name + '\''
+                + ", model='"
+                + model + '\''
+                + ", engine="
+                + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create user clone" + e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,54 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private double engineVolume;
+    private boolean isDiesel;
+    private boolean isRotary;
+    private int numberOfCylinders;
+    private int yearProduce;
 
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setDiesel(boolean diesel) {
+        isDiesel = diesel;
+    }
+
+    public void setRotary(boolean rotary) {
+        isRotary = rotary;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    public void setYearProduce(int yearProduce) {
+        this.yearProduce = yearProduce;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "engineVolume="
+                + engineVolume
+                + ", isDiesel="
+                + isDiesel
+                + ", isRotary="
+                + isRotary
+                + ", numberOfCylinders="
+                + numberOfCylinders
+                + ", yearProduce="
+                + yearProduce
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone name " + e);
+        }
+    }
 }
