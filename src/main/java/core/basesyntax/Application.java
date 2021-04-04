4e4ecb940ee692957d977ade43f113cf251675a1
Cyouffy <cyouffy@gmail.com>
2021-04-04 11:45:25 +0300

    Added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,21 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // test your code here ...
+        Engine engine = new Engine();
+        engine.setType("Single cylinder");
+        engine.setCR(17);
+        engine.setDisplacement(661);
+        engine.setBore(87);
+        engine.setStroke(110);
 
+        Car car = new Car("Audi", "red", 200, 4, 5);
+        car.setEngine(engine);
+
+        System.out.println(car);
+        Car carClone = car.clone();
+        engine.setType("Radial");
+
+        System.out.println(carClone);
+        System.out.println(car);
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,44 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String color;
+    private int speed;
+    private int numberOfPassengers;
+    private int numberOfDoors;
+    private Engine engine;
 
+    public Car(String model, String color, int speed, int numberOfPassengers, int numberOfDoors) {
+        this.model = model;
+        this.color = color;
+        this.speed = speed;
+        this.numberOfPassengers = numberOfPassengers;
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car cloneCar = (Car) super.clone();
+            cloneCar.setEngine(engine.clone());
+            return cloneCar;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "model='" + model + '\''
+                + ", color='" + color + '\''
+                + ", speed='" + speed + '\''
+                + ", numberOfPassengers='" + numberOfPassengers + '\''
+                + ", numberOfDoors='" + numberOfDoors + '\''
+                + ", engine" + engine + '}';
+    }
 }
