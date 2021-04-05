fab3a5ade39fb3856097ca3eebfde727c78f80ff
VladislavTetar <luxeon010203@gmail.com>
2021-04-05 18:55:09 +0300

    Added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,52 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String carBrand;
+    private String model;
+    private String number;
+    private int yearOfCarProduction;
+    private int fuelCapacity;
+    private Engine carEngine;
 
+    public Car(String carBrand, String model, String number,
+               int yearOfCarProduction, int fuelCapacity, Engine carEngine) {
+        this.carBrand = carBrand;
+        this.model = model;
+        this.number = number;
+        this.yearOfCarProduction = yearOfCarProduction;
+        this.fuelCapacity = fuelCapacity;
+        this.carEngine = carEngine;
+    }
+
+    @Override
+    public String toString() {
+        return "................................." + System.lineSeparator()
+                + "Car:" + System.lineSeparator()
+                + "Car brand = '" + carBrand + '\'' + System.lineSeparator()
+                + "Model = '" + model + '\'' + System.lineSeparator()
+                + "Number = '" + number + '\'' + System.lineSeparator()
+                + "Year of car production = " + yearOfCarProduction + System.lineSeparator()
+                + "Fuel capacity = " + fuelCapacity + System.lineSeparator()
+                + "- Car engine: " + carEngine
+                + System.lineSeparator();
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setCarEngine(carEngine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
+
+    private void setCarEngine(Engine carEngine) {
+        this.carEngine = carEngine;
+    }
+
+    public void setYearOfCarProduction(int yearOfCarProduction) {
+        this.yearOfCarProduction = yearOfCarProduction;
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String engineName;
+    private String engineBrand;
+    private int engineNumber;
+    private double volume;
+    private double fuelConsumption;
 
+    public Engine(String engineName, String engineBrand, int engineNumber,
+                  double volume, double fuelConsumption) {
+        this.engineName = engineName;
+        this.engineBrand = engineBrand;
+        this.engineNumber = engineNumber;
+        this.volume = volume;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setEngineBrand(String engineBrand) {
+        this.engineBrand = engineBrand;
+    }
+
+    @Override
+    public String toString() {
+        return System.lineSeparator()
+                + "  Engine name = '" + engineName + '\'' + System.lineSeparator()
+                + "  Engine brand = '" + engineBrand + '\'' + System.lineSeparator()
+                + "  Engine number = " + engineNumber + System.lineSeparator()
+                + "  Volume = " + volume + System.lineSeparator()
+                + "  Fuel consumption = " + fuelConsumption + System.lineSeparator()
+                + ".................................";
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine", e);
+        }
+    }
 }
