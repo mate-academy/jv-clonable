1c8b53db4e82a278d0411174d4d72c6bc86b3013
Darina <to.darina.to>
2021-04-05 17:15:55 +0300

    1) Create 5 fields for Car and Engine classes

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,60 @@
 package core.basesyntax;
 
 public class Car {
+    private int ageModel;
+    private int price;
+    private String color;
+    private String model;
+    private Engine engine;
 
+    public int getAge() {
+        return ageModel;
+    }
+
+    public void setAgeModel(int age) {
+        this.ageModel = age;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "ageModel=" + ageModel +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", engine=" + engine +
+                '}';
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,60 @@
 package core.basesyntax;
 
 public class Engine {
+private String kindMotor;
+private String typeOfFuel;
+private String color;
+private int numberOfKeys;
+private int numberOfCylinders;
 
+    public String getKindMotor() {
+        return kindMotor;
+    }
+
+    public void setKindMotor(String kindMotor) {
+        this.kindMotor = kindMotor;
+    }
+
+    public String getTypeOfFuel() {
+        return typeOfFuel;
+    }
+
+    public void setTypeOfFuel(String typeOfFuel) {
+        this.typeOfFuel = typeOfFuel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
+    }
+
+    public void setNumberOfKeys(int numberOfKeys) {
+        this.numberOfKeys = numberOfKeys;
+    }
+
+    public int getNumberOfCylinders() {
+        return numberOfCylinders;
+    }
+
+    public void setNumberOfCylinders(int numberOfCylinders) {
+        this.numberOfCylinders = numberOfCylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "kindMotor='" + kindMotor + '\'' +
+                ", typeOfFuel='" + typeOfFuel + '\'' +
+                ", color='" + color + '\'' +
+                ", numberOfKeys=" + numberOfKeys +
+                ", numberOfCylinders=" + numberOfCylinders +
+                '}';
+    }
 }
