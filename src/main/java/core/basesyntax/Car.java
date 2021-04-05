7fdbb466c7338c9ff2af177901a025dd5e931c9d
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-04-05 18:49:10 +0300

    first solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private String color;
+    private int registrationPlate;
+    private int doorsCount;
+    private boolean works;
 
+    public Car(Engine engine, String color, int registrationPlate, int doorsCount, boolean works) {
+        this.engine = engine;
+        this.color = color;
+        this.registrationPlate = registrationPlate;
+        this.doorsCount = doorsCount;
+        this.works = works;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.engine = engine.clone();
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("No clones here!", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "engine=" + engine
+                + ", color='" + color + '\''
+                + ", number=" + registrationPlate
+                + ", doorsCount=" + doorsCount
+                + ", works=" + works
+                + '}';
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private boolean works;
+    private int cylinderCount;
+    private int horsepower;
+    private boolean isDirty;
+    private String eats;
 
+    public Engine(boolean works, int cylinderCount, int horsepower, boolean isDirty, String eats) {
+        this.works = works;
+        this.cylinderCount = cylinderCount;
+        this.horsepower = horsepower;
+        this.isDirty = isDirty;
+        this.eats = eats;
+    }
+
+    public void setEats(String eats) {
+        this.eats = eats;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            Engine clonedEngine = (Engine) super.clone();
+            clonedEngine.setEats(eats);
+            return clonedEngine;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("No clones here!", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "works=" + works
+                + ", cylinderCount=" + cylinderCount
+                + ", horsepower=" + horsepower
+                + ", isDirty=" + isDirty
+                + ", eats='" + eats + '\''
+                + '}';
+    }
 }
