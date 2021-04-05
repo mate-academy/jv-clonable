0396655c4a3329e1ea77fbc86303d273a7df260f
Nazar-yo <bilaknazar@gmail.com>
2021-04-05 21:57:04 +0300

    All done

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,90 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private static final String DEFAULT_MODEL = "Vaz-28";
+    private static final String DEFAULT_COLOR = "Blue";
+    private static final double DEFAULT_WEIGHT = 7.8;
+    private static final double DEFAULT_MAXIMUM_SPEED = 120;
+    private static final int DEFAULT_ENGINE_CAPACITY = 1452;
+    private static final int DEFAULT_MAXIMUM_POWER = 68;
+    private static final String DEFAULT_USED_FUEL = "Gasoline";
+    private static final double DEFAULT_FUEL_CONSUMPTION = 7.8;
+    private static final int DEFAULT_NUMBER_OF_VALVES = 2;
 
+    private String model;
+    private String color;
+    private double weight;
+    private double maximumSpeed;
+    private Engine engine;
+
+    public Car() {
+        this.model = DEFAULT_MODEL;
+        this.color = DEFAULT_COLOR;
+        this.weight = DEFAULT_WEIGHT;
+        this.maximumSpeed = DEFAULT_MAXIMUM_SPEED;
+        this.engine = new Engine(DEFAULT_ENGINE_CAPACITY, DEFAULT_MAXIMUM_POWER,
+                DEFAULT_USED_FUEL, DEFAULT_FUEL_CONSUMPTION, DEFAULT_NUMBER_OF_VALVES);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    public void setMaximumSpeed(double maximumSpeed) {
+        this.maximumSpeed = maximumSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "model = " + model
+                + ", color= " + color
+                + ", weight = " + weight
+                + ", maximumSpeed = " + maximumSpeed
+                + ", engine = " + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone the car", e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,79 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int engineCapacity;
+    private int maximumPower;
+    private String usedFuel;
+    private double fuelConsumption;
+    private int numberOfValves;
 
+    public Engine(int engineCapacity, int maximumPower, String usedFuel,
+                  double fuelConsumption, int numberOfValves) {
+        this.engineCapacity = engineCapacity;
+        this.maximumPower = maximumPower;
+        this.usedFuel = usedFuel;
+        this.fuelConsumption = fuelConsumption;
+        this.numberOfValves = numberOfValves;
+    }
+
+    public int getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public int getMaximumPower() {
+        return maximumPower;
+    }
+
+    public void setMaximumPower(int maximumPower) {
+        this.maximumPower = maximumPower;
+    }
+
+    public String getUsedFuel() {
+        return usedFuel;
+    }
+
+    public void setUsedFuel(String usedFuel) {
+        this.usedFuel = usedFuel;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public void setNumberOfValves(int numberOfValves) {
+        this.numberOfValves = numberOfValves;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "engineCapacity = " + engineCapacity
+                + ", maximumPower = " + maximumPower
+                + ", usedFuel = " + usedFuel
+                + ", fuelConsumption = " + fuelConsumption
+                + ", numberOfValves = " + numberOfValves
+                + '}';
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone Engine", e);
+        }
+
+    }
 }
