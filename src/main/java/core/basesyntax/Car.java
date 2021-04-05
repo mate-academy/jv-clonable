5a6895704ff58918b762736a224e085bf396034b
Nikita <kupora3@gmail.com>
2021-04-05 22:32:29 +0300

    added solution

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,88 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private Engine engine;
+    private int countOfWheels;
+    private String model;
+    private String color;
+    private String name;
 
+    public Car(Engine engine, int countOfWheels, String model, String color, String name) {
+        this.engine = engine;
+        this.countOfWheels = countOfWheels;
+        this.model = model;
+        this.color = color;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "engine='"
+                + engine
+                + '\''
+                + ", countOfWheels="
+                + countOfWheels
+                + ", model="
+                + model
+                + ", color='"
+                + color
+                + '\''
+                + ", name='"
+                + name
+                + '\''
+                + '}';
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getCountOfWheels() {
+        return countOfWheels;
+    }
+
+    public void setCountOfWheels(int countOfWheels) {
+        this.countOfWheels = countOfWheels;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone object", e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,66 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private int horsePower;
+    private String color;
+    private String typeOfEngine;
+    private String stateOfEngine;
+    private int torque;
 
+    public Engine(int horsePower, String color,
+                  String typeOfEngine, String stateOfEngine, int torque) {
+        this.horsePower = horsePower;
+        this.color = color;
+        this.typeOfEngine = typeOfEngine;
+        this.stateOfEngine = stateOfEngine;
+        this.torque = torque;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "horsePower="
+                + horsePower
+                + ", color='"
+                + color
+                + '\''
+                + ", typeOfEngine='"
+                + typeOfEngine
+                + '\''
+                + ", stateOfEngine='"
+                + stateOfEngine
+                + '\''
+                + ", torque="
+                + torque
+                + '}';
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTypeOfEngine(String typeOfEngine) {
+        this.typeOfEngine = typeOfEngine;
+    }
+
+    public void setStateOfEngine(String stateOfEngine) {
+        this.stateOfEngine = stateOfEngine;
+    }
+
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Engine object", e);
+        }
+    }
 }
