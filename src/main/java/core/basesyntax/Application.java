5025e059e292f8b3dd4fce7112e3291875fcc329
peps <pepsithedoge@gmail.com>
2021-04-05 23:04:01 +0300

    not ready solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,15 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine twinHPower = new Engine("Twin H-Power", 6, 170,
+                85.6, true);
 
+        Car docHudsonHornet = new Car("Hudson Hornet", "deep dark blue", 1951,
+                twinHPower, true);
+
+        Car docsClone = docHudsonHornet.clone();
+        twinHPower.setPrice(86.0);
+        System.out.println(docHudsonHornet.toString());
+        System.out.println(docsClone.toString());
     }
 }
