98e13a609d0be5a52ec630c5082188061bc8505e
the5tranger <mishaantonyuk00@gmail.com>
2021-04-06 15:57:20 +0300

    I hope it will let go the first time

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine("SRS", "V8-TPS", 3,4, "gasoline");
+        Car car = new Car("subaru", "mechanical", 4, 2021, engine);
+        Car cloneCar = car.clone();
+        System.out.println(car);
+        System.out.println(cloneCar);
+        System.out.println(car.getEngine() == cloneCar.getEngine());
     }
 }
