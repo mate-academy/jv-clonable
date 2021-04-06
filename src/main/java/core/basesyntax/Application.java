dd98eff8c62f5fae77f9e58d8530cdfa280fa05d
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-06 21:45:12 +0300

    Ready for review.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine v12 = new Engine(6.3, 12, "petrol A-98",
+                "injector", 740);
+        Car ferrariF12Berlinetta = new Car("F12 Berlinetta", "Ferrari",
+                2012, 2, v12);
+        Car carClone = ferrariF12Berlinetta.clone();
+        v12.setMaximumHorsepower(780);
+        System.out.println(ferrariF12Berlinetta);
+        System.out.println(carClone);
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String manufacturer;
+    private int productionYear;
+    private int numberOfPassengers;
+    private Engine engine;
 
+    public Car(String model, String manufacturer, int productionYear,
+               int numberOfPassengers, Engine engine) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.productionYear = productionYear;
+        this.numberOfPassengers = numberOfPassengers;
+        this.engine = engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "model='" + model + '\''
+                + ", manufacturer='" + manufacturer + '\''
+                + ", productionYear=" + productionYear
+                + ", numberOfPassengers=" + numberOfPassengers
+                + ", engine=" + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t Cer clone", e);
+        }
+    }
 }
