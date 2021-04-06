c22456f0e8093be2ea869d20d73c6a034f1d122c
Alina <vasylyshyn.alina@gmail.com>
2021-04-07 01:48:37 +0300

    added method clone(), parameters differs in original and cloned car

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,71 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private int year;
+    private int mileAge;
+    private double speed;
+    private Engine engine;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileAge() {
+        return mileAge;
+    }
+
+    public void setMileAge(int mileAge) {
+        this.mileAge = mileAge;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "name='" + name + '\''
+                + ", year=" + year
+                + ", mileAge=" + mileAge
+                + ", speed=" + speed
+                + ", engine=" + engine
+                + '}';
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car cloneAudi = (Car) super.clone();
+            cloneAudi.setEngine(engine.clone());
+            return cloneAudi;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
 }
