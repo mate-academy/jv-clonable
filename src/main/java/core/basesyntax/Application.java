9fe7f4506716d512d0a92b26e23ba1ac0156cd21
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-04-06 17:32:02 +0300

    added solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,14 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
-
+        Engine engine = new Engine();
+        engine.setDiameter(88);
+        Car car = new Car("audi", "red", 1999, 220, engine);
+        Car carClone = car.clone();
+        System.out.println(car);
+        System.out.println(carClone);
+        car.getEngine().setDiameter(95);
+        System.out.println(car);
+        System.out.println(carClone);
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,77 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private int year;
+    private int maximumSpeed;
+    private Engine engine;
 
+    public Car(String name, String color, int year, int maximumSpeed, Engine engine) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+        this.maximumSpeed = maximumSpeed;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "name='" + name + '\''
+                + ", color='" + color + '\''
+                + ", year=" + year
+                + ", maximumSpeed=" + maximumSpeed
+                + ", numberOfSeats=" + engine + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    public void setMaximumSpeed(int maximumSpeed) {
+        this.maximumSpeed = maximumSpeed;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car carClone = (Car) super.clone();
+            carClone.setEngine(engine.clone());
+            return carClone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
 }
