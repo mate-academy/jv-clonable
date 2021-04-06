0261ba154fe24055fc17a2cda6a6c4dd655753d7
anufriiev <dimaanufriev1012@gmail.com>
2021-04-06 18:43:11 +0300

    Renamed object

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,12 +2,12 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        Engine audiV6 = new Engine("Petrol", 340, 15.0, 6,
+        Engine engineAudiV6 = new Engine("Petrol", 340, 15.0, 6,
                 "longitudinal");
         Car audi = new Car("Audi", "A7", 2015,
-                 "automatic", audiV6);
+                 "automatic", engineAudiV6);
         Car carClone = audi.clone();
-        audiV6.setType("diesel");
+        engineAudiV6.setType("diesel");
         carClone.setTransmission("manual");
         System.out.println(audi);
         System.out.println(carClone);
