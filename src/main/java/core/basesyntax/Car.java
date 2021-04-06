82d3753fb5614b14d31d82456a8acde93bd43460
Bohdan Borsh <borshbohdan@gmail.com>
2021-04-06 19:35:30 +0300

    Cloned classes created and tested.

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,74 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private Engine engine;
+    private String color;
+    private int year;
+    private int price;
 
+    public Car(String name, Engine engine, String color, int year, int price) {
+        this.name = name;
+        this.engine = engine;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return (color + " " + name + " with " + engine.toString()
+            + "\n" + "Made in " + year + ". Price - $" + price);
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone this car", e);
+        }
+    }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,73 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String manufacturer;
+    private String consumedFuelType;
+    private int cylindersNumber;
+    private double displacement;
+    private int horsePowers;
 
+    public Engine(String manufacturer, String consumedFuelType, int cylindersNumber,
+                  double displacement, int horsePowers) {
+        this.manufacturer = manufacturer;
+        this.consumedFuelType = consumedFuelType;
+        this.cylindersNumber = cylindersNumber;
+        this.displacement = displacement;
+        this.horsePowers = horsePowers;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getConsumedFuelType() {
+        return consumedFuelType;
+    }
+
+    public void setConsumedFuelType(String consumedFuelType) {
+        this.consumedFuelType = consumedFuelType;
+    }
+
+    public int getCylindersNumber() {
+        return cylindersNumber;
+    }
+
+    public void setCylindersNumber(int cylindersNumber) {
+        this.cylindersNumber = cylindersNumber;
+    }
+
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    public int getHorsePowers() {
+        return horsePowers;
+    }
+
+    public void setHorsePowers(int horsePowers) {
+        this.horsePowers = horsePowers;
+    }
+
+    @Override
+    public String toString() {
+        return (manufacturer + "'s " + cylindersNumber + "-cylinders engine");
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can`t clone this car", e);
+        }
+    }
 }
+
