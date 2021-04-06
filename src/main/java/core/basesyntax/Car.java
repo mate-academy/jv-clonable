594be226aaf3fe016c8d708b14297144830d429b
Mariia Chekmak <mariiachekmak@MacBook-Pro-Mariia.local>
2021-04-06 18:21:55 +0300

    completed task

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,80 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String model;
+    private String vin;
+    private int year;
+    private String equipment;
+    private String fuelType;
+    private Engine engine;
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cannot create car's clone", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car {"
+                + "model = '" + model + '\''
+                + ", year = " + year
+                + ", vin = '" + vin + '\''
+                + ", equipment = '" + equipment + '\''
+                + ", fuelType = '" + fuelType + '\''
+                + ", engine = " + engine + '}';
+    }
 }
