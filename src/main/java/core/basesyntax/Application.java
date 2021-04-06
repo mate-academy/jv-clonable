85824cb05c04bb09dcb9691a24e1b81c3f5d3b52
Anatolii Zarytskyi <tolia.zaritskiy@gmail.com>
2021-04-06 18:11:39 +0300

    hw6 is done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine("v8", "diesel", 400, true, true);
+        Car car = new Car("Ford", "white", 2018, true, engine);
 
+        Car clonedCar = car.clone();
+        car.getEngine().setName("v4");
+        car.getEngine().setType("electric");
+        System.out.println(clonedCar);
+        System.out.println(car);
     }
 }
--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,5 +1,79 @@
 package core.basesyntax;
 
-public class Car {
+public class Car implements Cloneable {
+    private String name;
+    private String color;
+    private int year;
+    private boolean isWorks;
+    private Engine engine;
 
+    public Car(String name, String color, int year, boolean isWorks, Engine engine) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+        this.isWorks = isWorks;
+        this.engine = engine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isWorks() {
+        return isWorks;
+    }
+
+    public void setWorks(boolean works) {
+        this.isWorks = works;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            Car clonedCar = (Car) super.clone();
+            clonedCar.setEngine(engine.clone());
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't create Car clone", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "name='" + name + '\''
+                + ", color='" + color + '\''
+                + ", year=" + year
+                + ", isWorks=" + isWorks
+                + ", engine=" + engine
+                + '}';
+    }
 }
