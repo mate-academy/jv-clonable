c4c0a522bb3ec89a0efc3b880a55e9139f112f7d
Stanislav Aleksandrovych <astanislav2361@gmail.com>
2021-04-06 16:00:12 +0300

    Task done

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,17 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // test your code here ...
+        Engine engine = new Engine("audi", "diesel", 2017, 230, 600);
+
+        Car car = new Car("yellow", "audi", 2017, 5.7, engine);
+
+        Car carClone = car.clone();
+
+        car.getEngine().setManufacturer("bmw");
+        car.getEngine().setSpeed(540);
+
+        System.out.println(car.toString());
+        System.out.println(carClone.toString());
 
     }
 }
