de1cc21f695b5d07652fe0ea4ac8be899a2c441d
oleksandr-shulha <shulgalex@gmail.com>
2021-04-06 15:43:48 +0300

    First edition

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,15 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
 
+        Engine engine = new Engine(1500, "BMW", 10000, 2018, true);
+        Car car = new Car(engine,"red", "BMW", 10000, 2018, true);
+        Car clonedCar = car.clone();
+        System.out.println("car " + car);
+        System.out.println("clonedCar " + clonedCar);
+        car.getEngine().setPower(2500);
+        System.out.println("Attention: we changed engine power!");
+        System.out.println("car " + car);
+        System.out.println("clonedCar " + clonedCar);
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,67 @@
 package core.basesyntax;
 
-public class Engine {
+class Engine implements Cloneable {
+    private int power;
+    private String manufacturer;
+    private int price;
+    private int year;
+    private boolean inStock;
+
+    public Engine(int power, String manufacturer,int price, int year, boolean inStock) {
+        this.power = power;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.year = year;
+        this.inStock = inStock;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    @Override
+    public Car clone() {
+        try {
+            return (Car) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cant clone the Car");
+        }
+    }
 
 }
