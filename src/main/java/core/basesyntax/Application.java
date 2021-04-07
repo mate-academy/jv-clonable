e03a84826787efaac0e9d94ea4d52a9d47e80ac3
AventusDev <aventus@ex.ua>
2021-04-07 14:03:04 +0300

    Add solution

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine v8 = new Engine(800, 3000, 25, 8, "v8");
+        Car zaporozhets = new Car("Zaporozhets", v8, 320, "ZAZ", 1999);
 
+        Car carClone = zaporozhets.clone();
+        v8.setConsumption(25);
+        v8.setHorsePower(3000);
+        System.out.println(carClone);
+        System.out.println(zaporozhets);
     }
 }
