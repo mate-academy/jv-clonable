86a6f52906863d13a06d31ee3e3789088c8cb988
Yaroslav Rybalko <yar.rybka@gmail.com>
2021-04-07 17:09:05 +0300

    resolved clone cars with the engines

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,7 +2,15 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine modificationForVolga = new Engine("ZMZ 2401 (A-76)", "USSR", 1970, 135, 2445);
+        Car scaldiaVolga = new Car("genosse Erich Honecker", "DDR", "GAZ-24", "USSR", 1973, modificationForVolga);
+
+        Car volga = scaldiaVolga.clone();
+        scaldiaVolga.getEngineModel().setModel("Indenor XDP 4.90");
+
+        System.out.println(scaldiaVolga.toString());
+        System.out.println(volga.toString());
+
 
     }
 }
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,5 +1,79 @@
 package core.basesyntax;
 
-public class Engine {
+public class Engine implements Cloneable {
+    private String model;
+    private String country;
+    private int year;
+    private int speed;
+    private int displacement;
 
+    public Engine(String model, String country, int year, int speed, int displacement) {
+        this.model = model;
+        this.country = country;
+        this.year = year;
+        this.speed = speed;
+        this.displacement = displacement;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    @Override
+    public String toString() {
+        return "------------------" + System.lineSeparator()
+                + " > Engine"  + System.lineSeparator()
+                + "------------------" + System.lineSeparator()
+                + "Model : " + model + System.lineSeparator()
+                + "Country : " + country + System.lineSeparator()
+                + "Year : " + year + System.lineSeparator()
+                + "Max. Speed : " + speed + System.lineSeparator()
+                + "Displacement : " + displacement + System.lineSeparator()
+                + "------------------" + System.lineSeparator();
+    }
+
+    @Override
+    protected Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Comrade, I can't clone the engine! Look: ", e);
+        }
+    }
 }
