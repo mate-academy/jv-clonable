351cccd6dd4d48416974ce6da0bc6d140eebf301
Koziarevskyi <koziarevskyi@gmail.com>
2021-04-07 04:16:36 +0300

    C002 - All Done!!! For Review.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,13 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
-        // test your code here ...
+        Engine engine = new Engine("Skoda", "TSI", 110, 6, 1499);
+        Car fabia2015White = new Car(engine, "Skoda", "Fabia", "White", 6, 8499);
+        Car fabia2010Red = fabia2015White.clone();
+        fabia2010Red.setAge(11);
+        fabia2010Red.setColor("Red");
 
-
+        System.out.println(fabia2015White);
+        System.out.println(fabia2010Red);
     }
 }
